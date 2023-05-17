6769888198bc41a0e83afc978f7ce2f774b32094
great-park <cksgh1735@gmail.com>
2023-05-18 03:56:55 +0900

    feat: Api Response Dto

---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/common/dto/ApiDataResponse.java
@@ -0,0 +1,27 @@
+package swm.hkcc.LGTM.app.common.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import swm.hkcc.LGTM.app.common.constant.ErrorCode;
+
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = true)
+public class ApiDataResponse<T> extends ApiResponse {
+
+    private final T data;
+
+    private ApiDataResponse(T data) {
+        super(true, ErrorCode.OK.getCode(), ErrorCode.OK.getMessage());
+        this.data = data;
+    }
+
+    public static <T> ApiDataResponse<T> of(T data) {
+        return new ApiDataResponse<>(data);
+    }
+
+    public static <T> ApiDataResponse<T> empty() {
+        return new ApiDataResponse<>(null);
+    }
+}
---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/common/dto/ApiResponse.java
@@ -0,0 +1,32 @@
+package swm.hkcc.LGTM.app.common.dto;
+
+
+import lombok.*;
+import swm.hkcc.LGTM.app.common.constant.ErrorCode;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class ApiResponse {
+
+    private final Boolean success;
+    private final Integer errorCode;
+    private final String message;
+
+    public static ApiResponse of(Boolean success, Integer errorCode, String message) {
+        return new ApiResponse(success, errorCode, message);
+    }
+
+    public static ApiResponse of(Boolean success, ErrorCode errorCode) {
+        return new ApiResponse(success, errorCode.getCode(), errorCode.getMessage());
+    }
+
+    public static ApiResponse of(Boolean success, ErrorCode errorCode, Exception e) {
+        return new ApiResponse(success, errorCode.getCode(), errorCode.getMessage(e));
+    }
+
+    public static ApiResponse of(Boolean success, ErrorCode errorCode, String message) {
+        return new ApiResponse(success, errorCode.getCode(), errorCode.getMessage(message));
+    }
+}
