a8af0a0063a3184cfacb91f553541a667b5792a6
great-park <cksgh1735@gmail.com>
2023-05-18 03:56:36 +0900

    feat: ErrorCode enum

---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/common/constant/ErrorCode.java
@@ -0,0 +1,65 @@
+package swm.hkcc.LGTM.app.common.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import swm.hkcc.LGTM.app.common.exception.GeneralException;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    OK(0, HttpStatus.OK, "Ok"),
+
+    BAD_REQUEST(10000, HttpStatus.BAD_REQUEST, "Bad request"),
+    NOT_FOUND(10001, HttpStatus.NOT_FOUND, "Requested resource is not found"),
+    VALIDATION_ERROR(10002, HttpStatus.BAD_REQUEST, "Validation error"),
+
+    INTERNAL_ERROR(20000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal error"),
+    DATA_ACCESS_ERROR(20001, HttpStatus.INTERNAL_SERVER_ERROR, "Data access error")
+    ;
+
+    private final Integer code;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+
+    public static ErrorCode valueOf(HttpStatus httpStatus) {
+        if (httpStatus == null) {
+            throw new GeneralException("HttpStatus is null.");
+        }
+
+        return Arrays.stream(values())
+                .filter(errorCode -> errorCode.getHttpStatus() == httpStatus)
+                .findFirst()
+                .orElseGet(() -> {
+                    if (httpStatus.is4xxClientError()) {
+                        return ErrorCode.BAD_REQUEST;
+                    } else if (httpStatus.is5xxServerError()) {
+                        return ErrorCode.INTERNAL_ERROR;
+                    } else {
+                        return ErrorCode.OK;
+                    }
+                });
+    }
+
+    public String getMessage(Throwable e) {
+        return this.getMessage(this.getMessage() + " - " + e.getMessage());
+    }
+
+    public String getMessage(String message) {
+        return Optional.ofNullable(message)
+                .filter(Predicate.not(String::isBlank))
+                .orElse(this.getMessage());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%d)", this.name(), this.getCode());
+    }
+
+}
