adfbd409f434288e873249986dbd9f4389124ee5
great-park <cksgh1735@gmail.com>
2023-05-18 03:22:24 +0900

    feat: init API Server

---
+++ API-Server/src/test/java/swm/hkcc/LGTM/LgtmApplicationTests.java
@@ -0,0 +1,13 @@
+package swm.hkcc.LGTM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LgtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
