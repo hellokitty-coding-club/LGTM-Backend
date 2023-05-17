286768dec62526f460f00eee4aab86f5ae613a92
great-park <cksgh1735@gmail.com>
2023-05-18 03:56:07 +0900

    feat: GeneralException

---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/common/exception/GeneralException.java
@@ -0,0 +1,50 @@
+package swm.hkcc.LGTM.app.common.exception;
+
+import lombok.Getter;
+import swm.hkcc.LGTM.app.common.constant.ErrorCode;
+
+@Getter
+public class GeneralException extends RuntimeException{
+
+    private final ErrorCode errorCode;
+
+    public GeneralException() {
+        super(ErrorCode.INTERNAL_ERROR.getMessage());
+        this.errorCode = ErrorCode.INTERNAL_ERROR;
+    }
+
+    public GeneralException(String message) {
+        super(ErrorCode.INTERNAL_ERROR.getMessage(message));
+        this.errorCode = ErrorCode.INTERNAL_ERROR;
+    }
+
+    public GeneralException(String message, Throwable cause) {
+        super(ErrorCode.INTERNAL_ERROR.getMessage(message), cause);
+        this.errorCode = ErrorCode.INTERNAL_ERROR;
+    }
+
+    public GeneralException(Throwable cause) {
+        super(ErrorCode.INTERNAL_ERROR.getMessage(cause));
+        this.errorCode = ErrorCode.INTERNAL_ERROR;
+    }
+
+    public GeneralException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public GeneralException(ErrorCode errorCode, String message) {
+        super(errorCode.getMessage(message));
+        this.errorCode = errorCode;
+    }
+
+    public GeneralException(ErrorCode errorCode, String message, Throwable cause) {
+        super(errorCode.getMessage(message), cause);
+        this.errorCode = errorCode;
+    }
+
+    public GeneralException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.getMessage(cause), cause);
+        this.errorCode = errorCode;
+    }
+}
