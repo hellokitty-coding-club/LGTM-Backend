b2a92220be60272c77672e9ea007c075a432bb8d
great-park <cksgh1735@gmail.com>
2023-06-26 17:46:20 +0900

    feat #2: get Github Access token & get Github User Information using access token, To do list 참고

--- API-Server/src/main/java/swm/hkcc/LGTM/app/controller/auth/OauthController.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/controller/auth/OauthController.java
@@ -2,16 +2,21 @@ package swm.hkcc.LGTM.app.controller.auth;
 
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import swm.hkcc.LGTM.app.dto.auth.GithubOAuthRequest;
+import swm.hkcc.LGTM.app.dto.auth.GithubOAuthResponse;
+import swm.hkcc.LGTM.app.dto.auth.GithubUserInfo;
 
 import java.net.URI;
+import java.util.LinkedHashMap;
 
 @Slf4j
 @RestController
@@ -24,14 +29,57 @@ public class OauthController {
     @Value("${spring.security.oauth2.client.registration.github.clientSecret}")
     private String githubClientSecret;
 
+    @Value("${spring.security.oauth2.client.registration.github.baseUri}")
+    private String githubBaseUri;
+
     @Value("${spring.security.oauth2.client.registration.github.redirectUri}")
     private String githubRedirectUri;
+
+    private final String getAccessTokenEndpoint = "https://github.com/login/oauth/access_token";
+
+    private final String getGithubUserInfoEndpoint = "https://api.github.com/user";
 
     @GetMapping(value = "/login/getGithubAuthUrl")
     public ResponseEntity<?> getGoogleAuthUrl(HttpServletRequest request) throws Exception {
         HttpHeaders headers = setGithubAuthRequestHeader();
 
         return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
+    }
+
+    @GetMapping(value = "/login/oauth2/code/github")
+    public ResponseEntity<?> githubAuthRedirectHandling (
+            @RequestParam(value = "code") String authCode,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        GithubOAuthRequest githubOAuthRequest = GithubOAuthRequest.builder()
+                .clientId(githubClientId)
+                .clientSecret(githubClientSecret)
+                .code(authCode)
+                .build();
+
+        RestTemplate restTemplate = new RestTemplate();
+        GithubOAuthResponse  githubOAuthResponse = restTemplate.postForObject(
+                getGithubAccessTokenRequestUrl(authCode),
+                githubOAuthRequest,
+                GithubOAuthResponse.class);
+
+        // Auth 서버로 부터 받은 githubOAuthResponse는 나중에 활용, 우선 access token만 사용
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(githubOAuthResponse.getAccessToken());
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        // To do: GithubUserInfoProvider 클래스를 만들어서, 여기서는 그냥 String으로 받아오고, 그 클래스에서 필요한 정보만 추출해서 리턴하도록 하기
+        // To do: 추출한 정보를 가지고 로그인과 회원가입 처리하기
+        // To do : 해당 메소드의 반환값은 우리 서버의 access token, refresh token, user 정보를 담은 dto일 것
+        return restTemplate.exchange(getGithubUserInfoEndpoint, HttpMethod.GET, entity, GithubUserInfo.class);
+    }
+
+    private String getGithubAccessTokenRequestUrl(String authCode) {
+        return getAccessTokenEndpoint
+                + "?client_id=" + githubClientId
+                + "&client_secret=" + githubClientSecret
+                + "&code=" + authCode;
     }
 
     private HttpHeaders setGithubAuthRequestHeader() {
@@ -43,7 +91,7 @@ public class OauthController {
     }
 
     private String setRequestUrl() {
-        return "https://github.com/login/oauth/authorize?client_id=" + githubClientId + "&redirect_uri=" + githubRedirectUri;
+        return githubBaseUri + "?client_id=" + githubClientId + "&redirect_uri=" + githubRedirectUri;
     }
 
 }
