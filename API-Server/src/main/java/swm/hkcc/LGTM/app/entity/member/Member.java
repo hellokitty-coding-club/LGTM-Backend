595b3b8db7982111d02d38808a74401ffc97f211
great-park <cksgh1735@gmail.com>
2023-06-26 15:25:30 +0900

    feat #2: Member & Authority Entity

---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/entity/member/Member.java
@@ -0,0 +1,43 @@
+package swm.hkcc.LGTM.app.entity.member;
+
+import jakarta.persistence.*;
+import lombok.*;
+import swm.hkcc.LGTM.app.global.entity.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "member", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<Authority> roles = new ArrayList<>();
+
+    public void setRoles(List<Authority> role) {
+        this.roles = role;
+        role.forEach(o -> o.setMember(this));
+    }
+}
