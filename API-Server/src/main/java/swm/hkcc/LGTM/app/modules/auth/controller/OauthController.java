fa6fdcdf9fe9bff4a678a1737ad1c755f9253a23
great-park <cksgh1735@gmail.com>
2023-06-27 18:16:39 +0900

    fix #2: change method

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/auth/controller/OauthController.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/auth/controller/OauthController.java
@@ -3,7 +3,6 @@ package swm.hkcc.LGTM.app.modules.auth.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -15,8 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import swm.hkcc.LGTM.app.global.dto.ApiDataResponse;
 import swm.hkcc.LGTM.app.modules.auth.dto.GithubOAuthResponse;
 import swm.hkcc.LGTM.app.modules.auth.dto.GithubUserInfo;
+import swm.hkcc.LGTM.app.modules.auth.dto.SignInResponse;
 import swm.hkcc.LGTM.app.modules.auth.service.GithubUserInfoProvider;
-import swm.hkcc.LGTM.app.modules.auth.service.UserAuthService;
+import swm.hkcc.LGTM.app.modules.auth.service.AuthService;
 
 @Slf4j
 @RestController
@@ -24,8 +24,7 @@ import swm.hkcc.LGTM.app.modules.auth.service.UserAuthService;
 public class OauthController {
 
     private final GithubUserInfoProvider githubUserInfoProvider;
-
-    private final UserAuthService userAuthService;
+    private final AuthService userAuthService;
 
     /**
      * Github 로그인 페이지로 리디렉션
@@ -43,22 +42,13 @@ public class OauthController {
      * Github의 유저 정보를 조회하여 로그인/회원가입 결과를 반환한다.
      */
     @GetMapping(value = "/login/oauth2/code/github")
-    public ApiDataResponse<?> githubAuthRedirectHandling(
-            @RequestParam(value = "code") String authCode,
-            HttpServletRequest request,
-            HttpServletResponse response
+    public ApiDataResponse<SignInResponse> githubAuthRedirectHandling(
+            @RequestParam(value = "code") String authCode
     ) {
-        // ② 로그인 후 리디렉션된 페이지에서 auth code를 받아옴
-
-        // ③ ~ ④ auth code를 이용해 access token(githubOAuthResponse.getAccessToken())을 받아옴
         GithubOAuthResponse githubOAuthResponse = githubUserInfoProvider.getGithubOAuthInfo(authCode);
-        // ⑤ ~ ⑥ access token을 이용해 github user 정보 조회
         GithubUserInfo githubUserInfo = githubUserInfoProvider.getGithubUserInfo(githubOAuthResponse.getAccessToken());
 
-        // 로그인 or 회원가입 정보 반환
-        return ApiDataResponse.of(
-                userAuthService.getUserLoginInfo(githubOAuthResponse, githubUserInfo)
-        );
+        return ApiDataResponse.of(userAuthService.handlingAuthentication(githubUserInfo, githubOAuthResponse));
     }
 
 }
