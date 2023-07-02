d1839e30093efba11290267cfd06eb7cd9d96102
great-park <cksgh1735@gmail.com>
2023-07-02 16:53:00 +0900

    refactor 2: refactor endpoints

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/auth/controller/OauthController.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/auth/controller/OauthController.java
@@ -1,54 +1,51 @@
 package swm.hkcc.LGTM.app.modules.auth.controller;
 
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import swm.hkcc.LGTM.app.global.dto.ApiDataResponse;
-import swm.hkcc.LGTM.app.modules.auth.dto.GithubOAuthResponse;
-import swm.hkcc.LGTM.app.modules.auth.dto.GithubUserInfo;
-import swm.hkcc.LGTM.app.modules.auth.dto.SignInResponse;
-import swm.hkcc.LGTM.app.modules.auth.service.GithubUserInfoProvider;
+import swm.hkcc.LGTM.app.modules.auth.dto.signIn.GithubSignInRequest;
+import swm.hkcc.LGTM.app.modules.auth.dto.oauth.GithubUserInfo;
+import swm.hkcc.LGTM.app.modules.auth.dto.signIn.SignInResponse;
+import swm.hkcc.LGTM.app.modules.auth.dto.signUp.JuniorSignUpRequest;
+import swm.hkcc.LGTM.app.modules.auth.dto.signUp.SeniorSignUpRequest;
+import swm.hkcc.LGTM.app.modules.auth.dto.signUp.SignUpResponse;
+import swm.hkcc.LGTM.app.modules.auth.utils.GithubUserInfoProvider;
 import swm.hkcc.LGTM.app.modules.auth.service.AuthService;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/v1/auth")
 public class OauthController {
 
     private final GithubUserInfoProvider githubUserInfoProvider;
-    private final AuthService userAuthService;
+    private final AuthService authService;
 
-    /**
-     * Github 로그인 페이지로 리디렉션
-     */
-    @GetMapping(value = "/login/getGithubAuthUrl")
-    public ResponseEntity<?> getGithubAuthUrl(HttpServletRequest request) throws Exception {
-        HttpHeaders headers = githubUserInfoProvider.getGithubAuthRequestHeader();
+    @GetMapping(value = "/githubSignIn")
+    public ApiDataResponse<SignInResponse> githubSignIn(
+            @Validated @RequestBody GithubSignInRequest request
+    ) {
+        GithubUserInfo githubUserInfo = githubUserInfoProvider
+                .getGithubUserInfo(request.getGithubAccessToken());
 
-        // ① 로그인 페이지로 리디렉션
-        return new ResponseEntity<>(headers, HttpStatus.FOUND);
+        return ApiDataResponse.of(authService.githubSignIn(githubUserInfo));
     }
 
-    /**
-     * Github 로그인 후 리디렉션
-     * Github의 유저 정보를 조회하여 로그인/회원가입 결과를 반환한다.
-     */
-    @GetMapping(value = "/login/oauth2/code/github")
-    public ApiDataResponse<SignInResponse> githubAuthRedirectHandling(
-            @RequestParam(value = "code") String authCode
+    @PostMapping("/junior/signup")
+    public ApiDataResponse<SignUpResponse> signup(
+            @Validated @RequestBody JuniorSignUpRequest request
     ) {
-        GithubOAuthResponse githubOAuthResponse = githubUserInfoProvider.getGithubOAuthInfo(authCode);
-        GithubUserInfo githubUserInfo = githubUserInfoProvider.getGithubUserInfo(githubOAuthResponse.getAccessToken());
+        return ApiDataResponse.of(authService.juniorSignUp(request));
+    }
 
-        return ApiDataResponse.of(userAuthService.handlingAuthentication(githubUserInfo, githubOAuthResponse));
+    @PostMapping("/senior/signup")
+    public ApiDataResponse<SignUpResponse> signup(
+            @Validated @RequestBody SeniorSignUpRequest request
+    ) {
+        return ApiDataResponse.of(authService.seniorSignUp(request));
     }
 
 }
