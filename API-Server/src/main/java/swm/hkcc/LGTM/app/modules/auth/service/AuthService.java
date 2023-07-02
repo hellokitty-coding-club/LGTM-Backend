11e5ec9edcb275adcccffa2f1da512f6aaf908e2
great-park <cksgh1735@gmail.com>
2023-07-02 16:52:30 +0900

    feature #2: refactor signIn method & implement signUp method with some private method

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/auth/service/AuthService.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/auth/service/AuthService.java
@@ -1,10 +1,16 @@
 package swm.hkcc.LGTM.app.modules.auth.service;
 
-import swm.hkcc.LGTM.app.modules.auth.dto.GithubOAuthResponse;
-import swm.hkcc.LGTM.app.modules.auth.dto.GithubUserInfo;
-import swm.hkcc.LGTM.app.modules.auth.dto.SignInResponse;
+import swm.hkcc.LGTM.app.modules.auth.dto.oauth.GithubUserInfo;
+import swm.hkcc.LGTM.app.modules.auth.dto.signIn.SignInResponse;
+import swm.hkcc.LGTM.app.modules.auth.dto.signUp.JuniorSignUpRequest;
+import swm.hkcc.LGTM.app.modules.auth.dto.signUp.SeniorSignUpRequest;
+import swm.hkcc.LGTM.app.modules.auth.dto.signUp.SignUpResponse;
 
 
 public interface AuthService {
-    SignInResponse handlingAuthentication(GithubUserInfo githubUserInfo, GithubOAuthResponse githubOAuthResponse);
+    SignInResponse githubSignIn(GithubUserInfo githubUserInfo);
+
+    SignUpResponse juniorSignUp(JuniorSignUpRequest request);
+
+    SignUpResponse seniorSignUp(SeniorSignUpRequest request);
 }
--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/auth/service/AuthServiceImpl.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/auth/service/AuthServiceImpl.java
@@ -2,14 +2,25 @@ package swm.hkcc.LGTM.app.modules.auth.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import swm.hkcc.LGTM.app.modules.auth.constants.TokenType;
-import swm.hkcc.LGTM.app.modules.auth.dto.*;
+import swm.hkcc.LGTM.app.modules.auth.dto.oauth.GithubUserInfo;
+import swm.hkcc.LGTM.app.modules.auth.dto.signIn.SignInResponse;
+import swm.hkcc.LGTM.app.modules.auth.dto.signUp.JuniorSignUpRequest;
+import swm.hkcc.LGTM.app.modules.auth.dto.signUp.SeniorSignUpRequest;
+import swm.hkcc.LGTM.app.modules.auth.dto.signUp.SignUpRequest;
+import swm.hkcc.LGTM.app.modules.auth.dto.signUp.SignUpResponse;
 import swm.hkcc.LGTM.app.modules.auth.utils.jwt.TokenProvider;
-import swm.hkcc.LGTM.app.modules.member.entity.Member;
+import swm.hkcc.LGTM.app.modules.member.domain.Authority;
+import swm.hkcc.LGTM.app.modules.member.domain.Junior;
+import swm.hkcc.LGTM.app.modules.member.domain.Member;
+import swm.hkcc.LGTM.app.modules.member.domain.Senior;
+import swm.hkcc.LGTM.app.modules.member.repository.JuniorRepository;
 import swm.hkcc.LGTM.app.modules.member.repository.MemberRepository;
+import swm.hkcc.LGTM.app.modules.member.repository.SeniorRepository;
 
+import java.util.Collections;
 import java.util.Optional;
 
 
@@ -19,42 +30,92 @@ import java.util.Optional;
 public class AuthServiceImpl implements AuthService {
 
     private final MemberRepository memberRepository;
+    private final JuniorRepository juniorRepository;
+    private final SeniorRepository seniorRepository;
     private final TokenProvider tokenProvider;
 
     @Override
-    public SignInResponse handlingAuthentication(GithubUserInfo githubUserInfo, GithubOAuthResponse githubOAuthResponse) {
+    @Transactional(readOnly = true)
+    public SignInResponse githubSignIn(GithubUserInfo githubUserInfo) {
+        log.info("githubUserInfo={}", githubUserInfo);
+
         Optional<Member> member = memberRepository.findOneByGithubId(githubUserInfo.getLogin());
 
         if (member.isPresent()) {
             return SignInResponse.builder()
-                    .userId(member.get().getMemberId())
+                    .memberId(member.get().getMemberId())
                     .githubId(githubUserInfo.getLogin())
                     .isRegistered(true)
-                    .tokenDto(TokenDetailDto.builder()
-                            .accessToken(tokenProvider.createToken(
-                                    member.get().getMemberId(),
-                                    member.get().getGithubId(),
-                                    TokenType.ACCESS_TOKEN
-                            ))
-                            .refreshToken(tokenProvider.createToken(
-                                            member.get().getMemberId(),
-                                            member.get().getGithubId(),
-                                            TokenType.REFRESH_TOKEN
-                                    ))
-                            .githubAccessToken(githubOAuthResponse.getAccessToken())
-                            .githubRefreshToken(githubOAuthResponse.getRefreshToken())
-                            .build())
-                    .build();
-        } else {
-            return SignInResponse.builder()
-                    .userId(0L)
-                    .githubId(githubUserInfo.getLogin())
-                    .isRegistered(false)
-                    .tokenDto(TokenDto.builder()
-                            .githubAccessToken(githubOAuthResponse.getAccessToken())
-                            .githubRefreshToken(githubOAuthResponse.getRefreshToken())
-                            .build())
+                    .accessToken(createAccessToken(member.get()))
+                    .refreshToken(createRefreshToken(member.get()))
                     .build();
         }
+
+        return SignInResponse.builder()
+                .memberId(0L)
+                .githubId(githubUserInfo.getLogin())
+                .isRegistered(false)
+                .build();
+
+    }
+
+    @Override
+    @Transactional
+    public SignUpResponse juniorSignUp(JuniorSignUpRequest request) {
+        Member member = createAndSaveMember(request);
+        Junior junior = Junior.create(request, member);
+        juniorRepository.save(junior);
+
+        return buildSignUpResponse(member);
+    }
+
+    @Override
+    @Transactional
+    public SignUpResponse seniorSignUp(SeniorSignUpRequest request) {
+        Member member = createAndSaveMember(request);
+        Senior senior = Senior.create(request, member);
+        seniorRepository.save(senior);
+
+        return buildSignUpResponse(member);
+    }
+
+    private Member createAndSaveMember(SignUpRequest request) {
+        Member member = Member.create(request);
+        member.setRoles(Collections.singletonList(Authority.builder().name("ROLE_USER").build()));
+        //tag list 추가
+        Member savedMember = memberRepository.save(member);
+        updateRefreshToken(savedMember);
+
+        return savedMember;
+    }
+
+    private String createAccessToken(Member member) {
+        return tokenProvider.createToken(
+                member.getMemberId(),
+                member.getGithubId(),
+                TokenType.ACCESS_TOKEN
+        );
+    }
+
+    private String createRefreshToken(Member member) {
+        return tokenProvider.createToken(
+                member.getMemberId(),
+                member.getGithubId(),
+                TokenType.REFRESH_TOKEN
+        );
+    }
+
+    private void updateRefreshToken(Member member) {
+        String refreshToken = createRefreshToken(member);
+        member.setRefreshToken(refreshToken);
+    }
+
+    private SignUpResponse buildSignUpResponse(Member member) {
+        return SignUpResponse.builder()
+                .memberId(member.getMemberId())
+                .githubId(member.getGithubId())
+                .accessToken(createAccessToken(member))
+                .refreshToken(member.getRefreshToken())
+                .build();
     }
 }
