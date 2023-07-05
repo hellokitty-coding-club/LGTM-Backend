3d5110ae515214b434939c17a02d43cdff8cd9ea
great-park <cksgh1735@gmail.com>
2023-07-05 17:00:21 +0900

    fix #2: @Repository 빈 등록

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/member/repository/MemberRepository.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/member/repository/MemberRepository.java
@@ -1,12 +1,16 @@
 package swm.hkcc.LGTM.app.modules.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import swm.hkcc.LGTM.app.modules.member.domain.Member;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findOneByNickName(String nickName);
 
     Optional<Member> findOneByGithubId(String githubId);
+
+    boolean existsByNickName(String nickName);
 }
--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/member/repository/SeniorRepository.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/member/repository/SeniorRepository.java
@@ -1,7 +1,9 @@
 package swm.hkcc.LGTM.app.modules.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import swm.hkcc.LGTM.app.modules.member.domain.Senior;
 
+@Repository
 public interface SeniorRepository extends JpaRepository<Senior, Long> {
 }
