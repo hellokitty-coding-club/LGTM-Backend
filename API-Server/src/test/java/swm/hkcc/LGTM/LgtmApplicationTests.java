adad8aa8ff00dc38b44104152451edb1f4e99ccf
ray-yhc <raycho521@yonsei.ac.kr>
2023-07-06 11:24:54 +0900

    test #3: add test for introapi

--- API-Server/src/test/java/swm/hkcc/LGTM/LgtmApplicationTests.java
+++ API-Server/src/test/java/swm/hkcc/LGTM/LgtmApplicationTests.java
@@ -1,13 +1,25 @@
 package swm.hkcc.LGTM;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import swm.hkcc.LGTM.app.global.api.IntroController;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(IntroController.class)
 class LgtmApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		mockMvc.perform(get("/api/v1/intro"))
+				.andExpect(status().isOk());
+
 	}
 
 }
---
+++ API-Server/src/test/java/swm/hkcc/LGTM/app/global/api/IntroControllerTest.java
@@ -0,0 +1,10 @@
+package swm.hkcc.LGTM.app.global.api;
+
+import org.junit.jupiter.api.Test;
+
+class IntroControllerTest {
+
+    @Test
+    void getIntro() {
+    }
+}
