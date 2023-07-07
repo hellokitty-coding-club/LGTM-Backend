fc5d3062aa8f86d8c472c740b57f53fe3c540be8
ray-yhc <raycho521@yonsei.ac.kr>
2023-07-07 20:45:53 +0900

    fix #3: fix test and docs

---
+++ API-Server/src/test/java/swm/hkcc/LGTM/LgtmApplicationTest.java
@@ -0,0 +1,12 @@
+package swm.hkcc.LGTM;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LgtmApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
