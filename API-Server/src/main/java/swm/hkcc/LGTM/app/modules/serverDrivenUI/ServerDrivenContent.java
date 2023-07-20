abd412a9e7a3a26e28b511af3fd68b70cc1c122a
soma-devops <cksgh1735@gmail.com>
2023-07-20 10:50:47 +0900

    feat #11: common Dto for Server Driven UI

---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/serverDrivenUI/ServerDrivenContent.java
@@ -0,0 +1,8 @@
+package swm.hkcc.LGTM.app.modules.serverDrivenUI;
+
+public class ServerDrivenContent<T> {
+
+    private String viewType;
+
+    private T content;
+}
---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/serverDrivenUI/ServerDrivenScreenResponse.java
@@ -0,0 +1,10 @@
+package swm.hkcc.LGTM.app.modules.serverDrivenUI;
+
+import java.util.List;
+
+public class ServerDrivenScreenResponse {
+
+    private String screenName;
+
+    private List<ServerDrivenContent<?>> contents;
+}
