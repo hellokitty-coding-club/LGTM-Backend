7085f91dd420fe766704bce6e1224c646e571df7
ray-yhc <raycho521@yonsei.ac.kr>
2023-07-20 15:45:05 +0900

    feat: add new controller for create mission

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/controller/CreateMissionController.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/controller/CreateMissionController.java
@@ -1,11 +1,35 @@
 package swm.hkcc.LGTM.app.modules.mission.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import swm.hkcc.LGTM.app.global.dto.ApiDataResponse;
+import swm.hkcc.LGTM.app.modules.member.repository.MemberRepository;
+import swm.hkcc.LGTM.app.modules.mission.dto.CreateMissionRequest;
+import swm.hkcc.LGTM.app.modules.mission.dto.CreateMissionResponse;
+import swm.hkcc.LGTM.app.modules.mission.service.CreateMissionService;
 
 @RestController
 @RequestMapping("/mission")
 public class CreateMissionController {
 
+    @Autowired
+    CreateMissionService createMissionService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @PostMapping
+    public ApiDataResponse<CreateMissionResponse> createMissinon(
+            Authentication authentication,
+            @Validated @RequestBody CreateMissionRequest requestBody
+    ) {
+        String memberGithubId = authentication.getName();
+        return ApiDataResponse.of(createMissionService.createMission(memberGithubId, requestBody));
+    }
 
 }
