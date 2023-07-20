92b9ddc497ee925a4006a2855dd1b77a823c04a9
ray-yhc <raycho521@yonsei.ac.kr>
2023-07-20 11:30:56 +0900

    feat: add CreateMissionController

---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/controller/CreateMissionController.java
@@ -0,0 +1,11 @@
+package swm.hkcc.LGTM.app.modules.mission.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mission")
+public class CreateMissionController {
+
+
+}
