95f617877a8bb831444103d0f2504f14df67aaa6
soma-devops <cksgh1735@gmail.com>
2023-07-23 22:53:22 +0900

    fix #11: 메소드 name 변경 & sequence factory - Component 등록

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/domain/serverDrivenUI/HomeServerDrivenUISequenceFactory.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/domain/serverDrivenUI/HomeServerDrivenUISequenceFactory.java
@@ -1,11 +1,13 @@
 package swm.hkcc.LGTM.app.modules.mission.domain.serverDrivenUI;
 
+import org.springframework.stereotype.Component;
 import swm.hkcc.LGTM.app.modules.mission.constant.HomeServerDrivenUISequenceByVersion;
 import swm.hkcc.LGTM.app.modules.serverDrivenUI.domain.ServerDrivenUISequenceFactory;
 
+@Component
 public class HomeServerDrivenUISequenceFactory implements ServerDrivenUISequenceFactory {
     @Override
-    public HomeServerDrivenUISequenceByVersion createServerDrivenUISequenceByVersion(int version) {
+    public HomeServerDrivenUISequenceByVersion getServerDrivenUISequenceByVersion(int version) {
         return null;
     }
 }
---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/domain/serverDrivenUI/MissionProcessServerDrivenUISequenceFactory.java
@@ -0,0 +1,11 @@
+package swm.hkcc.LGTM.app.modules.mission.domain.serverDrivenUI;
+
+import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenUISequenceByVersion;
+import swm.hkcc.LGTM.app.modules.serverDrivenUI.domain.ServerDrivenUISequenceFactory;
+
+public class MissionProcessServerDrivenUISequenceFactory implements ServerDrivenUISequenceFactory {
+    @Override
+    public ServerDrivenUISequenceByVersion getServerDrivenUISequenceByVersion(int version) {
+        return null;
+    }
+}
