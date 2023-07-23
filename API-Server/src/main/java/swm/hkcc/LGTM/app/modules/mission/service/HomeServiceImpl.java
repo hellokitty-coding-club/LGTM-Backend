a4b009de0766488a318ce395c259bbf4154f4e9c
soma-devops <cksgh1735@gmail.com>
2023-07-23 22:51:30 +0900

    fix #11: 메소드 name 변경

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/HomeServiceImpl.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/HomeServiceImpl.java
@@ -1,12 +1,25 @@
 package swm.hkcc.LGTM.app.modules.mission.service;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import swm.hkcc.LGTM.app.modules.mission.constant.HomeServerDrivenUISequenceByVersion;
+import swm.hkcc.LGTM.app.modules.mission.domain.serverDrivenUI.HomeServerDrivenUISequenceFactory;
+import swm.hkcc.LGTM.app.modules.mission.repository.MissionRepository;
 import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenScreenResponse;
 
+@Slf4j
+@RequiredArgsConstructor
 @Service
 public class HomeServiceImpl implements HomeService{
+
+    private final HomeServerDrivenUISequenceFactory homeServerDrivenUISequenceFactory;
+    private final MissionRepository missionRepository;
+    private final JPAQueryFactory jpaQueryFactory;
+
     @Override
-    public ServerDrivenScreenResponse getHomeScreen(String githubId, int version) {
+    public ServerDrivenScreenResponse getHomeScreen(Long memberId, int version) {
         return null;
     }
 }
