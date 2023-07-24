7d9cbaf840a1efdbf9e55ec75bf7f28169fe3d9c
ray-yhc <raycho521@yonsei.ac.kr>
2023-07-24 21:12:26 +0900

    feat: edit CreateMissionController

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/controller/CreateMissionController.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/controller/CreateMissionController.java
@@ -1,20 +1,21 @@
 package swm.hkcc.LGTM.app.modules.mission.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import swm.hkcc.LGTM.app.global.dto.ApiDataResponse;
+import swm.hkcc.LGTM.app.modules.member.domain.custom.CustomUserDetails;
 import swm.hkcc.LGTM.app.modules.member.repository.MemberRepository;
 import swm.hkcc.LGTM.app.modules.mission.dto.CreateMissionRequest;
 import swm.hkcc.LGTM.app.modules.mission.dto.CreateMissionResponse;
 import swm.hkcc.LGTM.app.modules.mission.service.CreateMissionService;
 
 @RestController
-@RequestMapping("/mission")
+@RequestMapping("/v1/mission")
 public class CreateMissionController {
 
     @Autowired
@@ -25,11 +26,11 @@ public class CreateMissionController {
 
     @PostMapping
     public ApiDataResponse<CreateMissionResponse> createMissinon(
-            Authentication authentication,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @Validated @RequestBody CreateMissionRequest requestBody
     ) {
-        String memberGithubId = authentication.getName();
-        return ApiDataResponse.of(createMissionService.createMission(memberGithubId, requestBody));
+        Long memberId = customUserDetails.getMemberId();
+        return ApiDataResponse.of(createMissionService.createMission(memberId, requestBody));
     }
 
 }
