6e7c41b4a3abc1958c12a47de5dc3bc4e211b4ce
soma-devops <cksgh1735@gmail.com>
2023-07-24 16:29:53 +0900

    refactor #11: static import MissionContentType

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/constant/HomeServerDrivenUISequenceByVersion.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/constant/HomeServerDrivenUISequenceByVersion.java
@@ -1,5 +1,4 @@
 package swm.hkcc.LGTM.app.modules.mission.constant;
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenUISequenceByVersion;
@@ -8,31 +7,33 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static swm.hkcc.LGTM.app.modules.mission.constant.MissionContentType.*;
+
 @Getter
 @RequiredArgsConstructor
 public enum HomeServerDrivenUISequenceByVersion implements ServerDrivenUISequenceByVersion {
     V1_HOME_SERVER_DRIVEN_UI_SEQUENCE(
             1,
-            List.of(MissionContentType.ON_GOING_MISSION_TITLE,
-                    MissionContentType.ON_GOING_MISSION_LIST,
-                    MissionContentType.SECTION_CLOSER,
-                    MissionContentType.RECOMMENDED_MISSION_TITLE,
-                    MissionContentType.RECOMMENDED_MISSION_LIST,
-                    MissionContentType.SECTION_CLOSER,
-                    MissionContentType.TOTAL_MISSION_TITLE,
-                    MissionContentType.TOTAL_MISSION_LIST
-                    )),
+            List.of(ON_GOING_MISSION_TITLE,
+                    ON_GOING_MISSION_LIST,
+                    SECTION_CLOSER,
+                    RECOMMENDED_MISSION_TITLE,
+                    RECOMMENDED_MISSION_LIST,
+                    SECTION_CLOSER,
+                    TOTAL_MISSION_TITLE,
+                    TOTAL_MISSION_LIST
+            )),
 
     V2_HOME_SERVER_DRIVEN_UI_SEQUENCE(
             2,
-            List.of(MissionContentType.RECOMMENDED_MISSION_TITLE,
-            MissionContentType.RECOMMENDED_MISSION_LIST,
-            MissionContentType.SECTION_CLOSER,
-            MissionContentType.ON_GOING_MISSION_TITLE,
-            MissionContentType.ON_GOING_MISSION_LIST,
-            MissionContentType.SECTION_CLOSER,
-            MissionContentType.TOTAL_MISSION_TITLE,
-            MissionContentType.TOTAL_MISSION_LIST
+            List.of(RECOMMENDED_MISSION_TITLE,
+                    RECOMMENDED_MISSION_LIST,
+                    SECTION_CLOSER,
+                    ON_GOING_MISSION_TITLE,
+                    ON_GOING_MISSION_LIST,
+                    SECTION_CLOSER,
+                    TOTAL_MISSION_TITLE,
+                    TOTAL_MISSION_LIST
             ));
 
     private final int version;
