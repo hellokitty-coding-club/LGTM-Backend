3f9a42dc1b48876ee9df582d43568df51e1f7859
soma-devops <cksgh1735@gmail.com>
2023-07-25 16:29:44 +0900

    feat #11: Server Driven UI에서 사용할 content 내 실제 DB에서 가져온 값을 담을 Dto

---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/dto/MissionDetailsDto.java
@@ -0,0 +1,28 @@
+package swm.hkcc.LGTM.app.modules.mission.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import swm.hkcc.LGTM.app.modules.tag.domain.TechTag;
+
+import java.util.List;
+
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class MissionDetailsDto {
+    private Long missionId;
+    private String missionTitle;
+    private List<TechTag> techTagList;
+    private String missionThumbnailUrl;
+    private int remainingRegisterDays;
+    private int viewCount;
+    private int price;
+    private int currentPeopleNumber;
+    private int maxPeopleNumber;
+
+    private boolean isScraped;
+
+    private int scrapCount;
+}
