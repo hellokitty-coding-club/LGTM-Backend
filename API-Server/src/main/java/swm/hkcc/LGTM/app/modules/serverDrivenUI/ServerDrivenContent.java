7bdb618aa321233dbfd90b796b3ee6fb60b8ebb5
soma-devops <cksgh1735@gmail.com>
2023-07-25 16:32:02 +0900

    feat #11: Server Drieven UI에서 사용하는 content list를 담은 일급 컬렉션 정의 및 사용

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/serverDrivenUI/ServerDrivenContent.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/serverDrivenUI/ServerDrivenContent.java
@@ -1,10 +1,24 @@
 package swm.hkcc.LGTM.app.modules.serverDrivenUI;
 
+import lombok.Builder;
+import lombok.Getter;
+import swm.hkcc.LGTM.app.modules.mission.dto.MissionDto;
+
+@Builder
+@Getter
 public class ServerDrivenContent<T> {
 
-    private String viewType;
+    private ViewType viewType;
 
     private Theme theme;
 
     private T content;
+
+    public static ServerDrivenContent from(MissionDto missionDto, Theme theme, ViewType viewType) {
+        return ServerDrivenContent.builder()
+                .viewType(viewType)
+                .theme(theme)
+                .content(missionDto)
+                .build();
+    }
 }
