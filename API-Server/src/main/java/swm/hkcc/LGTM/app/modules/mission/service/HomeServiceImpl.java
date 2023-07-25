b070efd2f51bee0f8e00f5163773ab7d503479c7
soma-devops <cksgh1735@gmail.com>
2023-07-25 16:36:09 +0900

    feat #11: homse service에서 version에 따른 content sequence에 맞춰서 ServerDrivenContents들을 조립하여 최종적으로 ServerDrivenScreenResponse을 만든다.

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/HomeServiceImpl.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/HomeServiceImpl.java
@@ -1,12 +1,11 @@
 package swm.hkcc.LGTM.app.modules.mission.service;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import swm.hkcc.LGTM.app.modules.mission.constant.HomeServerDrivenUISequenceByVersion;
+import swm.hkcc.LGTM.app.modules.mission.domain.MissionContentSequence;
 import swm.hkcc.LGTM.app.modules.mission.domain.serverDrivenUI.HomeServerDrivenUISequenceFactory;
-import swm.hkcc.LGTM.app.modules.mission.repository.MissionRepository;
+import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenContents;
 import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenScreenResponse;
 
 @Slf4j
@@ -15,11 +14,18 @@ import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenScreenResponse;
 public class HomeServiceImpl implements HomeService{
 
     private final HomeServerDrivenUISequenceFactory homeServerDrivenUISequenceFactory;
-    private final MissionRepository missionRepository;
-    private final JPAQueryFactory jpaQueryFactory;
+    private final MissionService missionService;
 
     @Override
     public ServerDrivenScreenResponse getHomeScreen(Long memberId, int version) {
+        MissionContentSequence contentSequence = homeServerDrivenUISequenceFactory.createMissionContentSequenceFromVersion(version);
+
+        ServerDrivenContents onGoingMissionList = missionService.getOngoingMissions(memberId);
+        ServerDrivenContents recommendMissionList = missionService.getRecommendMissions(memberId);
+        ServerDrivenContents totalMissionList = missionService.getTotalMissions(memberId);
+
+        // todo: sequence에 맞춰서 최종적인 ServerDrivenScreenResponse 만들기
+
         return null;
     }
 }
