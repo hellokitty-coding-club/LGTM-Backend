1e3b49d4f29dce7d7826f00b7d4a2c6b87b0ae8b
soma-devops <cksgh1735@gmail.com>
2023-07-27 16:15:03 +0900

    feat #11: mission에 대한 DB접근을 담당하는 service layer - 모든 미션 가져오기 구현, 추천 미션은 구현 미완료로 모든 미션 가져오기로 대체

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/MissionServiceImpl.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/MissionServiceImpl.java
@@ -3,8 +3,10 @@ package swm.hkcc.LGTM.app.modules.mission.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import swm.hkcc.LGTM.app.modules.mission.dto.MissionDetailsDto;
 import swm.hkcc.LGTM.app.modules.mission.dto.MissionDto;
 import swm.hkcc.LGTM.app.modules.mission.repository.MissionCustomRepository;
+import swm.hkcc.LGTM.app.modules.mission.repository.MissionRepository;
 import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenContent;
 import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenContents;
 import swm.hkcc.LGTM.app.modules.serverDrivenUI.Theme;
@@ -17,10 +19,12 @@ import java.util.List;
 @Service
 public class MissionServiceImpl implements MissionService {
 
+    private final MissionRepository missionRepository;
     private final MissionCustomRepository missionCustomRepository;
 
     @Override
     public ServerDrivenContents getOngoingMissions(Long memberId) {
+//        validateMemberId(memberId);
         List<MissionDto> missions = missionCustomRepository.getOnGoingMissions(memberId);
 
         return ServerDrivenContents.of(
@@ -29,13 +33,24 @@ public class MissionServiceImpl implements MissionService {
                         .toList());
     }
 
+    // todo: 추천 미션 가져오기 구현 미완료 -> 전체 미션 가져오기로 임시 대체
     @Override
     public ServerDrivenContents getRecommendMissions(Long memberId) {
-        return null;
+        List<MissionDetailsDto> missions = missionCustomRepository.getTotalMissions(memberId);
+
+        return ServerDrivenContents.of(
+                missions.stream()
+                        .map(missionDetailsDto -> ServerDrivenContent.from(missionDetailsDto, Theme.LIGHT, ViewType.ITEM))
+                        .toList());
     }
 
     @Override
     public ServerDrivenContents getTotalMissions(Long memberId) {
-        return null;
+        List<MissionDetailsDto> missions = missionCustomRepository.getTotalMissions(memberId);
+
+        return ServerDrivenContents.of(
+                missions.stream()
+                        .map(missionDetailsDto -> ServerDrivenContent.from(missionDetailsDto, Theme.LIGHT, ViewType.ITEM))
+                        .toList());
     }
 }
