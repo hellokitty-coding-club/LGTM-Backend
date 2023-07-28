0cb867849dfc5211d19128caef4c7574f921b816
soma-devops <cksgh1735@gmail.com>
2023-07-28 16:04:30 +0900

    refactor #11: holder 패턴과 함수형 인터페이스를 활용하여 역할 분리 - switch문 제거

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/HomeServiceImpl.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/HomeServiceImpl.java
@@ -3,15 +3,18 @@ package swm.hkcc.LGTM.app.modules.mission.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import swm.hkcc.LGTM.app.global.constant.ResponseCode;
-import swm.hkcc.LGTM.app.global.exception.GeneralException;
+import swm.hkcc.LGTM.app.modules.mission.constant.MissionContentType;
 import swm.hkcc.LGTM.app.modules.mission.domain.MissionContentSequence;
+import swm.hkcc.LGTM.app.modules.mission.domain.holder.MissionItemHolder;
+import swm.hkcc.LGTM.app.modules.mission.domain.holder.MissionTitleHolder;
 import swm.hkcc.LGTM.app.modules.mission.domain.serverDrivenUI.HomeServerDrivenUISequenceFactory;
-import swm.hkcc.LGTM.app.modules.mission.dto.MissionTitleDto;
-import swm.hkcc.LGTM.app.modules.serverDrivenUI.*;
+import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenContent;
+import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenContents;
+import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenScreenResponse;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,39 +22,30 @@ import java.util.List;
 public class HomeServiceImpl implements HomeService{
 
     private final HomeServerDrivenUISequenceFactory sequenceFactory;
-    private final MissionService missionService;
+    private final MissionItemHolder missionItemHolder;
+    private final MissionTitleHolder missionTitleHolder;
 
-    private static final String TOTAL_MISSION_TITLE_TEXT = "더 많은 미션 찾아보기";
-    private static final String ONGOING_MISSION_TITLE_TEXT = "진행중인 미션";
-    private static final String RECOMMENDED_MISSION_TITLE_TEXT = "맞춤 추천 미션 목록이에요";
     private static final String RESPONSE_SCREEN_NAME = "Home";
 
     @Override
     public ServerDrivenScreenResponse getHomeScreen(Long memberId, int version) {
         MissionContentSequence contentSequence = sequenceFactory.getServerDrivenUISequenceByVersion(version);
         List<ServerDrivenContent> serverDrivenContentList = new ArrayList<>();
+
         contentSequence.getMissionContents()
-                .forEach(missionContentType -> {
-                    switch (missionContentType) {
-                        case TOTAL_MISSION_TITLE -> serverDrivenContentList.add(getMissionTitle(TOTAL_MISSION_TITLE_TEXT, Theme.LIGHT));
-                        case ON_GOING_MISSION_TITLE -> serverDrivenContentList.add(getMissionTitle(ONGOING_MISSION_TITLE_TEXT, Theme.DARK));
-                        case RECOMMENDED_MISSION_TITLE -> serverDrivenContentList.add(getMissionTitle(RECOMMENDED_MISSION_TITLE_TEXT, Theme.LIGHT));
-                        case TOTAL_MISSION_LIST -> addMissionList(missionService.getTotalMissions(memberId), serverDrivenContentList);
-                        case ON_GOING_MISSION_LIST -> addMissionList(missionService.getOngoingMissions(memberId), serverDrivenContentList);
-                        case RECOMMENDED_MISSION_LIST -> addMissionList(missionService.getRecommendMissions(memberId), serverDrivenContentList);
-                        case SECTION_CLOSER -> serverDrivenContentList.add(ServerDrivenContent.from(Theme.LIGHT, ViewType.CLOSER));
-                        default -> throw new GeneralException(ResponseCode.DATA_ACCESS_ERROR, missionContentType.getClass().getName());
-                    }
-                });
+                .forEach(missionContentType -> processMissionContentType(memberId, missionContentType, serverDrivenContentList));
 
         return new ServerDrivenScreenResponse(RESPONSE_SCREEN_NAME, serverDrivenContentList);
     }
 
-    private ServerDrivenContent getMissionTitle(String title, Theme theme) {
-        MissionTitleDto missionTitleDto = MissionTitleDto.builder()
-                .title(title)
-                .build();
-        return ServerDrivenContent.from(missionTitleDto, theme, ViewType.TITLE);
+    private void processMissionContentType(Long memberId, MissionContentType missionContentType, List<ServerDrivenContent> serverDrivenContentList) {
+        Function<Long, ServerDrivenContents> missionListFunction = missionItemHolder.getMissionListFunction(missionContentType);
+
+        if (missionListFunction != null) {
+            addMissionList(missionListFunction.apply(memberId), serverDrivenContentList);
+            return;
+        }
+        serverDrivenContentList.add(missionTitleHolder.getMissionTitle(missionContentType));
     }
 
     private void addMissionList(ServerDrivenContents missionContents, List<ServerDrivenContent> contentList) {
