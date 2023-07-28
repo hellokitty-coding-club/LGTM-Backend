a69f9e3c3c004be1664a5020a9e5802d0c8c89ff
soma-devops <cksgh1735@gmail.com>
2023-07-28 18:05:20 +0900

    fix #11: 문자열 상수로 분리 & lombok 활용

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/domain/holder/MissionTitleHolder.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/domain/holder/MissionTitleHolder.java
@@ -1,5 +1,7 @@
 package swm.hkcc.LGTM.app.modules.mission.domain.holder;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import swm.hkcc.LGTM.app.modules.mission.constant.MissionContentType;
 import swm.hkcc.LGTM.app.modules.mission.dto.MissionTitleDto;
@@ -8,22 +10,23 @@ import swm.hkcc.LGTM.app.modules.serverDrivenUI.Theme;
 import swm.hkcc.LGTM.app.modules.serverDrivenUI.ViewType;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Component
 public class MissionTitleHolder {
+    private static final String ON_GOING_MISSION_TITLE_NAME = "진행 중인 미션";
+    private static final String RECOMMENDED_MISSION_TITLE_NAME = "맞춤 추천 미션 목록이에요";
+    private static final String TOTAL_MISSION_TITLE_NAME = "더 많은 미션 찾아보기";
 
     private static final Map<MissionContentType, MissionTitleDetails> TITLE_DETAILS_MAP = Map.of(
-            MissionContentType.ON_GOING_MISSION_TITLE, MissionTitleDetails.of("진행중인 미션", Theme.DARK),
-            MissionContentType.RECOMMENDED_MISSION_TITLE, MissionTitleDetails.of("맞춤 추천 미션 목록이에요", Theme.LIGHT),
-            MissionContentType.TOTAL_MISSION_TITLE, MissionTitleDetails.of("더 많은 미션 찾아보기", Theme.LIGHT)
+            MissionContentType.ON_GOING_MISSION_TITLE, MissionTitleDetails.of(ON_GOING_MISSION_TITLE_NAME, Theme.DARK),
+            MissionContentType.RECOMMENDED_MISSION_TITLE, MissionTitleDetails.of(RECOMMENDED_MISSION_TITLE_NAME, Theme.LIGHT),
+            MissionContentType.TOTAL_MISSION_TITLE, MissionTitleDetails.of(TOTAL_MISSION_TITLE_NAME, Theme.LIGHT)
     );
 
     public ServerDrivenContent getMissionTitle(MissionContentType missionContentType) {
-        MissionTitleDetails details = TITLE_DETAILS_MAP.get(missionContentType);
-
-        if (details == null) {
-            return null;
-        }
+        MissionTitleDetails details = Optional.ofNullable(TITLE_DETAILS_MAP.get(missionContentType))
+                .orElseThrow(() -> new IllegalArgumentException("Invalid MissionContentType: " + missionContentType));
 
         MissionTitleDto missionTitleDto = MissionTitleDto.builder()
                 .title(details.getTitle())
@@ -31,25 +34,14 @@ public class MissionTitleHolder {
         return ServerDrivenContent.from(missionTitleDto, details.getTheme(), ViewType.TITLE);
     }
 
+    @Getter
+    @RequiredArgsConstructor
     private static class MissionTitleDetails {
         private final String title;
         private final Theme theme;
 
-        private MissionTitleDetails(String title, Theme theme) {
-            this.title = title;
-            this.theme = theme;
-        }
-
         public static MissionTitleDetails of(String title, Theme theme) {
             return new MissionTitleDetails(title, theme);
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public Theme getTheme() {
-            return theme;
         }
     }
 }
