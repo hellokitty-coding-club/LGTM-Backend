24e1b1e537de4f986203ae62db21e59aa97edac7
ray-yhc <raycho521@yonsei.ac.kr>
2023-07-28 18:38:29 +0900

    fix: 비회원 응답에 image url 추가

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/auth/service/AuthServiceImpl.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/auth/service/AuthServiceImpl.java
@@ -77,6 +77,7 @@ public class AuthServiceImpl implements AuthService {
                 .githubId(githubUserInfo.getLogin())
                 .githubOauthId(githubUserInfo.getId())
                 .isRegistered(false)
+                .profileImageUrl(githubUserInfo.getAvatar_url())
                 .build();
 
     }
