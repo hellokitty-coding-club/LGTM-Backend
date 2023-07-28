aa88fe0ef6f96da0e42a2ebc0a9f552aa0ac7d1b
soma-devops <cksgh1735@gmail.com>
2023-07-28 18:10:45 +0900

    feat #11: Mission Item에 대해서 처리할 때, 만약 contents가 없다면 EMPTY view를 생성해서 삽입한다.

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/HomeServiceImpl.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/HomeServiceImpl.java
@@ -3,14 +3,14 @@ package swm.hkcc.LGTM.app.modules.mission.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import swm.hkcc.LGTM.app.modules.mission.constant.MissionContentType;
 import swm.hkcc.LGTM.app.modules.mission.domain.MissionContentSequence;
+import swm.hkcc.LGTM.app.modules.mission.domain.holder.MissionCloserHolder;
 import swm.hkcc.LGTM.app.modules.mission.domain.holder.MissionItemHolder;
 import swm.hkcc.LGTM.app.modules.mission.domain.holder.MissionTitleHolder;
 import swm.hkcc.LGTM.app.modules.mission.domain.serverDrivenUI.HomeServerDrivenUISequenceFactory;
-import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenContent;
-import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenContents;
-import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenScreenResponse;
+import swm.hkcc.LGTM.app.modules.serverDrivenUI.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,13 +21,16 @@ import java.util.function.Function;
 @Service
 public class HomeServiceImpl implements HomeService{
 
+    public static final MissionContentType DARK_MISSION_CONTENT_TYPE = MissionContentType.ON_GOING_MISSION_LIST;
     private final HomeServerDrivenUISequenceFactory sequenceFactory;
     private final MissionItemHolder missionItemHolder;
     private final MissionTitleHolder missionTitleHolder;
+    private final MissionCloserHolder missionCloserHolder;
 
     private static final String RESPONSE_SCREEN_NAME = "Home";
 
     @Override
+    @Transactional(readOnly = true)
     public ServerDrivenScreenResponse getHomeScreen(Long memberId, int version) {
         MissionContentSequence contentSequence = sequenceFactory.getServerDrivenUISequenceByVersion(version);
         List<ServerDrivenContent> serverDrivenContentList = new ArrayList<>();
@@ -39,13 +42,32 @@ public class HomeServiceImpl implements HomeService{
     }
 
     private void processMissionContentType(Long memberId, MissionContentType missionContentType, List<ServerDrivenContent> serverDrivenContentList) {
-        Function<Long, ServerDrivenContents> missionListFunction = missionItemHolder.getMissionListFunction(missionContentType);
-
-        if (missionListFunction != null) {
-            addMissionList(missionListFunction.apply(memberId), serverDrivenContentList);
+        if (missionContentType.getViewType() == ViewType.ITEM) {
+            Function<Long, ServerDrivenContents> missionListFunction = missionItemHolder.getMissionListFunction(missionContentType);
+            ServerDrivenContents missionContents = missionListFunction.apply(memberId);
+            addMissionContentsOrEmptyView(serverDrivenContentList, missionContents, missionContentType);
             return;
         }
-        serverDrivenContentList.add(missionTitleHolder.getMissionTitle(missionContentType));
+        if (missionContentType.getViewType() == ViewType.TITLE) {
+            serverDrivenContentList.add(missionTitleHolder.getMissionTitle(missionContentType));
+            return;
+        }
+        serverDrivenContentList.add(missionCloserHolder.getMissionCloser(missionContentType));
+    }
+
+    private void addMissionContentsOrEmptyView(List<ServerDrivenContent> serverDrivenContentList, ServerDrivenContents missionContents, MissionContentType missionContentType) {
+        if (missionContents.getContents().isEmpty()) {
+            addEmptyView(serverDrivenContentList, missionContentType);
+            return;
+        }
+        addMissionList(missionContents, serverDrivenContentList);
+    }
+
+    private static void addEmptyView(List<ServerDrivenContent> serverDrivenContentList, MissionContentType missionContentType) {
+        if (missionContentType == DARK_MISSION_CONTENT_TYPE)
+            serverDrivenContentList.add(ServerDrivenContent.from(Theme.DARK, ViewType.EMPTY));
+        else
+            serverDrivenContentList.add(ServerDrivenContent.from(Theme.LIGHT, ViewType.EMPTY));
     }
 
     private void addMissionList(ServerDrivenContents missionContents, List<ServerDrivenContent> contentList) {
