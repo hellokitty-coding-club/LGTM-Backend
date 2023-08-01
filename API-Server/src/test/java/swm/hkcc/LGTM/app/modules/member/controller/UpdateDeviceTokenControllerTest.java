eddcb4e52dfc74927332560039e8f1e049992c04
ray-yhc <raycho521@yonsei.ac.kr>
2023-08-01 19:33:45 +0900

    doc: update device token controller doc

--- API-Server/src/test/java/swm/hkcc/LGTM/app/modules/member/controller/UpdateDeviceTokenControllerTest.java
+++ API-Server/src/test/java/swm/hkcc/LGTM/app/modules/member/controller/UpdateDeviceTokenControllerTest.java
@@ -1,6 +1,8 @@
 package swm.hkcc.LGTM.app.modules.member.controller;
 
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -24,10 +26,13 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import swm.hkcc.LGTM.app.global.constant.ResponseCode;
 import swm.hkcc.LGTM.app.modules.member.domain.Authority;
 import swm.hkcc.LGTM.app.modules.member.domain.Member;
 import swm.hkcc.LGTM.app.modules.member.domain.custom.CustomUserDetails;
+import swm.hkcc.LGTM.app.modules.member.exception.NotExistMember;
 import swm.hkcc.LGTM.app.modules.member.service.MemberService;
+import swm.hkcc.LGTM.utils.CustomMDGenerator;
 
 import java.util.Collections;
 
@@ -35,6 +40,7 @@ import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.docume
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
@@ -42,6 +48,8 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static swm.hkcc.LGTM.utils.CustomMDGenerator.tableHead;
+import static swm.hkcc.LGTM.utils.CustomMDGenerator.tableRow;
 
 @Slf4j
 @SpringBootTest
@@ -104,8 +112,26 @@ class UpdateDeviceTokenControllerTest {
                         preprocessResponse(prettyPrint()),       // response JSON 정렬하여 출력
 
                         resource(ResourceSnippetParameters.builder()
-                                .summary("디바이스 토큰 업데이트")
-                                .description("디바이스 토큰 업데이트")
+                                .summary("[멤버] 디바이스 토큰 업데이트")
+                                .description(
+                                        CustomMDGenerator.builder()
+                                                .h1("[Descriptions]")
+                                                .h3("디바이스 토큰 업데이트")
+                                                .h1("[Request Parameters]")
+                                                .table(
+                                                        tableHead("Name", "Type", "Description"),
+                                                        tableRow("deviceToken", "String", "디바이스 토큰")
+                                                )
+                                                .h1("[Errors]")
+                                                .table(
+                                                        tableHead("HTTP Status", "Response Code", "Message"),
+                                                        tableRow(
+                                                                ResponseCode.NOT_EXIST_MEMBER.getHttpStatus().toString(),
+                                                                ResponseCode.NOT_EXIST_MEMBER.getCode().toString(),
+                                                                ResponseCode.NOT_EXIST_MEMBER.getMessage())
+                                                )
+                                                .build()
+                                )
                                 .tag("멤버")
                                 .queryParameters(
                                         parameterWithName("deviceToken").description("디바이스 토큰")
@@ -118,5 +144,42 @@ class UpdateDeviceTokenControllerTest {
                                 )
                                 .build()
                         )));
+    }
+
+
+    @Test
+    @DisplayName("디바이스 토큰 업데이트 실패 테스트 - 존재하지 않는 회원")
+    void createMissionNotExistMember() throws Exception {
+        // given
+        Mockito.when(memberService.updateDeviceToken(Mockito.anyLong(), Mockito.anyString()))
+                .thenThrow(new NotExistMember());
+
+        // when
+
+        // then
+        ResponseCode expectedResponseCode = ResponseCode.NOT_EXIST_MEMBER;
+        ResultActions actions = mockMvc.perform(patch("/v1/member/device-token")
+                        .queryParam("deviceToken", "testDeviceToken")
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.responseCode").value(expectedResponseCode.getCode()))
+                .andExpect(jsonPath("$.message").value(expectedResponseCode.getMessage()));
+
+        // document
+        actions
+                .andDo(document("update-device-token-not-exist-member",
+                        preprocessRequest(prettyPrint()),        // request JSON 정렬하여 출력
+                        preprocessResponse(prettyPrint()),       // response JSON 정렬하여 출력
+
+                        resource(ResourceSnippetParameters.builder()
+                                .responseFields(
+                                        fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공 여부"),
+                                        fieldWithPath("responseCode").type(JsonFieldType.NUMBER).description("응답 코드"),
+                                        fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지")
+
+                                ).build())
+                ));
     }
 }
