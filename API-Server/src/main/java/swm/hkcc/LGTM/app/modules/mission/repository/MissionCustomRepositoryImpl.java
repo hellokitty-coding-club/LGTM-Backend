76cefbfe3a9c1fff0d090fa4a303159bbab82021
soma-devops <cksgh1735@gmail.com>
2023-08-02 14:33:02 +0900

    refactor #11: mission repository에서는 Mission 엔티티를 반환하도록 변경, Mission Service에서 좀 더 많은 역할을 수행하도록 변경 - repository에서 MissionDto를 완성해서 반환하는 것이 아니라 Service에서 필요한 정보를 repository에서 가져오고 결과를 조립한다.

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/repository/MissionCustomRepositoryImpl.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/repository/MissionCustomRepositoryImpl.java
@@ -6,20 +6,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import swm.hkcc.LGTM.app.modules.mission.domain.Mission;
-import swm.hkcc.LGTM.app.modules.mission.dto.MissionDetailsDto;
-import swm.hkcc.LGTM.app.modules.mission.dto.MissionDto;
 import swm.hkcc.LGTM.app.modules.registration.domain.PersonalStatus;
-import swm.hkcc.LGTM.app.modules.registration.domain.repository.MissionRegistrationRepository;
-import swm.hkcc.LGTM.app.modules.tag.domain.TechTag;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import static swm.hkcc.LGTM.app.modules.member.domain.QMember.member;
 import static swm.hkcc.LGTM.app.modules.mission.domain.QMission.mission;
 import static swm.hkcc.LGTM.app.modules.registration.domain.QMissionRegistration.missionRegistration;
-import static swm.hkcc.LGTM.app.modules.tag.domain.QTechTagPerMission.techTagPerMission;
 
 @Slf4j
 @Repository
@@ -27,18 +20,20 @@ import static swm.hkcc.LGTM.app.modules.tag.domain.QTechTagPerMission.techTagPer
 public class MissionCustomRepositoryImpl implements MissionCustomRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
-    private final MissionRepository missionRepository;
-    private final MissionScrapRepository missionScrapRepository;
-    private final MissionViewRepository missionViewRepository;
-    private final MissionRegistrationRepository missionRegistrationRepository;
 
     @Override
-    public List<MissionDto> getOnGoingMissions(Long memberId) {
-        List<Mission> onGoingMissions = getMissions(isMemberParticipating(memberId), isNotFinished());
+    public List<Mission> getOnGoingMissions(Long memberId) {
+        return getMissions(isMemberParticipating(memberId), isNotFinished());
+    }
 
-        return onGoingMissions.stream()
-                .map(this::toMissionDto)
-                .toList();
+    @Override // todo: get recommended missions
+    public List<Mission> getRecommendedMissions(Long memberId) {
+        return null;
+    }
+
+    @Override
+    public List<Mission> getTotalMissions(Long memberId) {
+        return getMissions(isNotFinished());
     }
 
     private List<Mission> getMissions(BooleanExpression isParticipating, BooleanExpression isNotFinished) {
@@ -60,66 +55,12 @@ public class MissionCustomRepositoryImpl implements MissionCustomRepository {
                 .fetch();
     }
 
-
     private BooleanExpression isMemberParticipating(Long memberId) {
         return member.memberId.eq(memberId);
     }
 
     private BooleanExpression isNotFinished() {
         return missionRegistration.status.ne(PersonalStatus.MISSION_FINISHED);
-    }
-
-    private MissionDto toMissionDto(Mission ongoingMission) {
-        List<TechTag> techTags = getTechTagList(ongoingMission.getMissionId());
-        return MissionDto.builder()
-                .missionId(ongoingMission.getMissionId())
-                .missionTitle(ongoingMission.getTitle())
-                .techTagList(techTags)
-                .missionThumbnailUrl(ongoingMission.getThumbnailImageUrl())
-                .build();
-    }
-
-    private List<TechTag> getTechTagList(Long missionId) {
-        return jpaQueryFactory
-                .select(techTagPerMission.techTag)
-                .from(techTagPerMission)
-                .where(techTagPerMission.mission.missionId.eq(missionId))
-                .fetch();
-    }
-
-    // todo: get recommended missions
-    @Override
-    public List<MissionDetailsDto> getRecommendedMissions(Long memberId) {
-        return null;
-    }
-
-    @Override
-    public List<MissionDetailsDto> getTotalMissions(Long memberId) {
-        List<Mission> missions = getMissions(isNotFinished());
-
-        return missions.stream()
-                .map(mission -> getMissionDetails(memberId, mission))
-                .toList();
-    }
-
-    private MissionDetailsDto getMissionDetails(Long memberId, Mission mission) {
-        return MissionDetailsDto.builder()
-                .missionId(mission.getMissionId())
-                .missionTitle(mission.getTitle())
-                .techTagList(getTechTagList(mission.getMissionId()))
-                .missionThumbnailUrl(mission.getThumbnailImageUrl())
-                .remainingRegisterDays(remainingRegisterDays(mission))
-                .viewCount(missionViewRepository.countByMission_MissionId(mission.getMissionId()))
-                .price(mission.getPrice())
-                .currentPeopleNumber(missionRegistrationRepository.countByMission_MissionId(mission.getMissionId()))
-                .maxPeopleNumber(mission.getMaxPeopleNumber())
-                .isScraped(missionScrapRepository.existsByScrapper_MemberIdAndMission_MissionId(memberId, mission.getMissionId()))
-                .scrapCount(missionScrapRepository.countByMission_MissionId(mission.getMissionId()))
-                .build();
-    }
-
-    private int remainingRegisterDays(Mission mission) {
-        return (int) ChronoUnit.DAYS.between(LocalDate.now(), mission.getRegistrationDueDate());
     }
 
 }
--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/MissionServiceImpl.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/MissionServiceImpl.java
@@ -3,16 +3,19 @@ package swm.hkcc.LGTM.app.modules.mission.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import swm.hkcc.LGTM.app.modules.mission.domain.Mission;
 import swm.hkcc.LGTM.app.modules.mission.domain.MissionContentData;
 import swm.hkcc.LGTM.app.modules.mission.dto.MissionDetailsDto;
 import swm.hkcc.LGTM.app.modules.mission.dto.MissionDto;
 import swm.hkcc.LGTM.app.modules.mission.repository.MissionCustomRepository;
-import swm.hkcc.LGTM.app.modules.mission.repository.MissionRepository;
-import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenContent;
-import swm.hkcc.LGTM.app.modules.serverDrivenUI.ServerDrivenContents;
-import swm.hkcc.LGTM.app.modules.serverDrivenUI.Theme;
-import swm.hkcc.LGTM.app.modules.serverDrivenUI.ViewType;
+import swm.hkcc.LGTM.app.modules.mission.repository.MissionScrapRepository;
+import swm.hkcc.LGTM.app.modules.mission.repository.MissionViewRepository;
+import swm.hkcc.LGTM.app.modules.registration.domain.repository.MissionRegistrationRepository;
+import swm.hkcc.LGTM.app.modules.tag.domain.TechTag;
+import swm.hkcc.LGTM.app.modules.tag.repository.TechTagPerMissionRepository;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Slf4j
@@ -21,22 +24,77 @@ import java.util.List;
 public class MissionServiceImpl implements MissionService {
 
     private final MissionCustomRepository missionCustomRepository;
+    private final MissionScrapRepository missionScrapRepository;
+    private final MissionViewRepository missionViewRepository;
+    private final MissionRegistrationRepository missionRegistrationRepository;
+    private final TechTagPerMissionRepository techTagPerMissionRepository;
 
     @Override
     public MissionContentData getOngoingMissions(Long memberId) {
-        List<MissionDto> missions = missionCustomRepository.getOnGoingMissions(memberId);
-        return MissionContentData.of(missions);
+        List<Mission> missions = missionCustomRepository.getOnGoingMissions(memberId);
+
+        return MissionContentData.of(
+                missions.stream()
+                        .map(this::toMissionDto)
+                        .toList()
+        );
     }
 
     @Override // todo: 추천 미션 가져오기 구현 미완료 -> 전체 미션 가져오기로 임시 대체
     public MissionContentData getRecommendMissions(Long memberId) {
-        List<MissionDetailsDto> missions = missionCustomRepository.getTotalMissions(memberId);
-        return MissionContentData.of(missions);
+        List<Mission> missions = missionCustomRepository.getTotalMissions(memberId);
+
+        return MissionContentData.of(
+                missions.stream()
+                        .map(mission -> toMissionDetailDto(memberId, mission))
+                        .toList()
+        );
     }
 
     @Override
     public MissionContentData getTotalMissions(Long memberId) {
-        List<MissionDetailsDto> missions = missionCustomRepository.getTotalMissions(memberId);
-        return MissionContentData.of(missions);
+        List<Mission> missions = missionCustomRepository.getTotalMissions(memberId);
+
+        return MissionContentData.of(
+                missions.stream()
+                        .map(mission -> toMissionDetailDto(memberId, mission))
+                        .toList()
+        );
+    }
+
+    private MissionDto toMissionDto(Mission ongoingMission) {
+        List<TechTag> techTags = techTagPerMissionRepository.findTechTagsByMissionId(ongoingMission.getMissionId());
+        return MissionDto.builder()
+                .missionId(ongoingMission.getMissionId())
+                .missionTitle(ongoingMission.getTitle())
+                .techTagList(techTags)
+                .missionThumbnailUrl(ongoingMission.getThumbnailImageUrl())
+                .build();
+    }
+
+    private MissionDetailsDto toMissionDetailDto(Long memberId, Mission mission) {
+        List<TechTag> techTags = techTagPerMissionRepository.findTechTagsByMissionId(mission.getMissionId());
+        int viewCount = missionViewRepository.countByMission_MissionId(mission.getMissionId());
+        int currentPeopleNumber = missionRegistrationRepository.countByMission_MissionId(mission.getMissionId());
+        boolean isScraped = missionScrapRepository.existsByScrapper_MemberIdAndMission_MissionId(memberId, mission.getMissionId());
+        int scrapCount = missionScrapRepository.countByMission_MissionId(mission.getMissionId());
+
+        return MissionDetailsDto.builder()
+                .missionId(mission.getMissionId())
+                .missionTitle(mission.getTitle())
+                .techTagList(techTags)
+                .missionThumbnailUrl(mission.getThumbnailImageUrl())
+                .remainingRegisterDays(remainingRegisterDays(mission))
+                .viewCount(viewCount)
+                .price(mission.getPrice())
+                .currentPeopleNumber(currentPeopleNumber)
+                .maxPeopleNumber(mission.getMaxPeopleNumber())
+                .isScraped(isScraped)
+                .scrapCount(scrapCount)
+                .build();
+    }
+
+    private int remainingRegisterDays(Mission mission) {
+        return (int) ChronoUnit.DAYS.between(LocalDate.now(), mission.getRegistrationDueDate());
     }
 }
