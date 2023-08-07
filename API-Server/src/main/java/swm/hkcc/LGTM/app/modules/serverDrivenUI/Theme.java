fb86a256bf664aa0fd5b3fdeabc7cd687769bb5d
great-park <cksgh1735@gmail.com>
2023-08-07 15:00:04 +0900

    fix #11: change Theme color

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/serverDrivenUI/Theme.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/serverDrivenUI/Theme.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Theme {
-    DARK("#000010", "#000020", "#000030", "dark"),
-    LIGHT("#FFFFF0", "#FFFFF1", "#FFFFF2", "light");
+    DARK("#030c1b", "#56ee9b", "#030c1b", "dark"),
+    LIGHT("#FFFFFF", "#000000", "#e7ecf2", "light");
 
     private final String backgroundColor;
     private final String titleColor;
