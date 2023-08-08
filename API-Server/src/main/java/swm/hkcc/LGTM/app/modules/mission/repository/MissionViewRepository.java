a281c329bc5f6cd405c839cdd32c1e68eacf42b2
great-park <cksgh1735@gmail.com>
2023-08-08 15:53:17 +0900

    refactor #31: mission 조회수 caching 적용

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/repository/MissionViewRepository.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/repository/MissionViewRepository.java
@@ -1,8 +1,10 @@
 package swm.hkcc.LGTM.app.modules.mission.repository;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import swm.hkcc.LGTM.app.modules.mission.domain.MissionView;
 
 public interface MissionViewRepository extends JpaRepository<MissionView, Long> {
+    @Cacheable(value = "mission_view_count", key = "#missionId")
     int countByMission_MissionId(Long missionId);
 }
