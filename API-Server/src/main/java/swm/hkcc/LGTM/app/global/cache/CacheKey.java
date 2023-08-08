9d293f62bf2b1cf330e7ef0dfceccecde34e13ae
great-park <cksgh1735@gmail.com>
2023-08-08 15:49:16 +0900

    feat #31: cache key 추가

--- API-Server/src/main/java/swm/hkcc/LGTM/app/global/cache/CacheKey.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/global/cache/CacheKey.java
@@ -10,7 +10,11 @@ public enum CacheKey {
     TECH_TAG_PER_MEMBER("tech_tag_per_member", ONE_DAY.getExpiryTimeSec()),
     TECH_TAG_PER_MISSION("tech_tag_per_mission", ONE_DAY.getExpiryTimeSec()),
     MISSION_PARTICIPANT_COUNT("mission_participant_count", ONE_DAY.getExpiryTimeSec()),
-
+    ON_GOING_MISSIONS("on_going_missions", ONE_MIN.getExpiryTimeSec()),
+    RECOMMENDED_MISSIONS("recommended_missions", ONE_MIN.getExpiryTimeSec()),
+    TOTAL_MISSIONS("total_missions", ONE_MIN.getExpiryTimeSec()),
+    MISSION_SCRAP_COUNT("mission_scrap_count", ONE_MIN.getExpiryTimeSec()),
+    MISSION_VIEW_COUNT("mission_view_count", ONE_MIN.getExpiryTimeSec()),
     ;
 
     private final String key;
