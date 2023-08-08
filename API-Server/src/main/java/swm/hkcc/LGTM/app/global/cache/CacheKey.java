61cabb0cfcf93de94653c7f5f28b471d82154dd4
great-park <cksgh1735@gmail.com>
2023-08-08 14:11:02 +0900

    fix #31: cache key - TTL 별로 구체화

--- API-Server/src/main/java/swm/hkcc/LGTM/app/global/cache/CacheKey.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/global/cache/CacheKey.java
@@ -4,9 +4,16 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum CacheKey {
-    LONG("long", 60 * 60),
-    SHORT("short", 60 * 1),
-    MEDIUM("medium", 60 * 5);
+    PERMANENT("permanent", 0),
+    ONE_MONTH("one_month", 60 * 60 * 24 * 30),
+    ONE_WEEK("one_week", 60 * 60 * 24 * 7),
+    ONE_DAY("one_day", 60 * 60 * 24),
+    FIVE_HOUR("five_hour", 60 * 60 * 5),
+    ONE_HOUR("one_hour", 60 * 60),
+    TEN_MIN("ten_min", 60 * 10),
+    FIVE_MIN("five_min", 60 * 5),
+    ONE_MIN("one_min", 60 * 1)
+    ;
 
     private final String key;
     private final int expiryTimeSec;
