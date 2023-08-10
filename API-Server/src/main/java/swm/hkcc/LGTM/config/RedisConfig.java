b6de08f665382babb75049846b8f66b827ba9a58
great-park <cksgh1735@gmail.com>
2023-08-10 15:31:26 +0900

    fix #29: non-cluster mode인 Redis는 Test Profile에서만 동작하도록 변경

--- API-Server/src/main/java/swm/hkcc/LGTM/config/RedisConfig.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
@@ -25,6 +26,7 @@ import java.util.stream.Collectors;
 @Configuration
 @EnableCaching
 @EnableRedisRepositories
+@Profile("test")
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
@@ -50,7 +52,7 @@ public class RedisConfig {
     }
 
     @Bean
-    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory){
+    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
         RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
                 .disableCachingNullValues()
                 .computePrefixWith(CacheKeyPrefix.simple())
