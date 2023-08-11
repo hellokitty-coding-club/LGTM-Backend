0ab3dcd47508cdde442bb497966c010a6563b115
great-park <cksgh1735@gmail.com>
2023-08-11 14:57:29 +0900

    feat #40: Invalid한 인가 정보에 대한 custom 에외

--- API-Server/src/main/java/swm/hkcc/LGTM/app/global/constant/ResponseCode.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/global/constant/ResponseCode.java
@@ -25,9 +25,9 @@ public enum ResponseCode {
     INVALID_TECH_TAG(10005, HttpStatus.BAD_REQUEST, "유효하지 않은 기술 태그입니다. 다시 시도해주세요."),
     INVALID_CAREER_PERIOD(10006, HttpStatus.BAD_REQUEST, "유효하지 않은 경력 기간입니다. 경력 기간은 최소 12달 이상이어야 합니다."),
     INVALID_BANK_NAME(10007, HttpStatus.BAD_REQUEST, "유효하지 않은 은행 이름입니다. 다시 시도해주세요."),
-    INVALID_AUTHENTICATION(10007, HttpStatus.BAD_REQUEST, "유효하지 않은 인증 정보입니다. 다시 시도해주세요."),
+    INVALID_AUTHENTICATION(10007, HttpStatus.UNAUTHORIZED, "유효하지 않은 인증 정보입니다. 토큰이 만료되었거나 유효하지 않은 토큰입니다."),
     UNSPECIFIED_MEMBER_TYPE(10008, HttpStatus.INTERNAL_SERVER_ERROR, "회원이 시니어, 주니어 둘 다 속하지 않습니다. 회원가입 과정에서 문제가 발생했을 가능성이 있습니다."),
-
+    INVALID_ROLE(10009, HttpStatus.FORBIDDEN, "유효하지 않은 인가 정보입니다. 사용자 권한을 확인해 주세요."),
 
 
     // Member Authority
@@ -36,7 +36,8 @@ public enum ResponseCode {
     NOT_JUNIOR_MEMBER(10102, HttpStatus.BAD_REQUEST, "주니어 회원만 접근할 수 있습니다."),
 
     INTERNAL_ERROR(20000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal error"),
-    DATA_ACCESS_ERROR(20001, HttpStatus.INTERNAL_SERVER_ERROR, "Data access error");
+    DATA_ACCESS_ERROR(20001, HttpStatus.INTERNAL_SERVER_ERROR, "Data access error")
+    ;
 
     private final Integer code;
     private final HttpStatus httpStatus;
