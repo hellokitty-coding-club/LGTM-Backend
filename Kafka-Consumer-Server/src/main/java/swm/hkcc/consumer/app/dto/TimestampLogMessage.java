7e5059bfc740ff6db43510f9151f8001b5051239
ray-yhc <raycho521@yonsei.ac.kr>
2023-08-16 14:57:56 +0900

    feat: add log messages

---
+++ Kafka-Consumer-Server/src/main/java/swm/hkcc/consumer/app/dto/TimestampLogMessage.java
@@ -0,0 +1,33 @@
+package swm.hkcc.consumer.app.dto;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Builder
+@ToString
+@Getter
+public class TimestampLogMessage {
+    private String eventLogType;
+    private String screenName;
+    private String target;
+    private Long missionId;
+    private Long memberId;
+    private LocalDateTime timestamp;
+
+    private TimestampLogMessage() {
+    }
+
+    private TimestampLogMessage(String eventLogType, String screenName, String target, Long missionId, Long memberId, LocalDateTime timestamp) {
+        this.eventLogType = eventLogType;
+        this.screenName = screenName;
+        this.target = target;
+        this.missionId = missionId;
+        this.memberId = memberId;
+        this.timestamp = timestamp;
+    }
+
+}
