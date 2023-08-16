0192d8186cc30835289434d25dd7d1064e377397
great-park <cksgh1735@gmail.com>
2023-08-16 14:44:36 +0900

    feat #47: groupName을 가져오는 Service 로직

---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/serverDrivenUI/domain/ABTestService.java
@@ -0,0 +1,17 @@
+package swm.hkcc.LGTM.app.modules.serverDrivenUI.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ABTestService {
+    private final ABTestUserGroupRepository abTestUserGroupRepository;
+
+    public String getGroupName(Long memberId, String testName) {
+        return abTestUserGroupRepository.findGroupNameByMemberIdAndTestName(memberId, testName)
+                .orElse(null);
+    }
+}
