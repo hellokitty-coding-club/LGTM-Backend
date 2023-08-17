55966be3f703d01bb20e24cd5fe3096054d7c98c
ray-yhc <raycho521@yonsei.ac.kr>
2023-08-17 22:51:00 +0900

    feat: add github url validation

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/CreateMissionServiceImpl.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/CreateMissionServiceImpl.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 import swm.hkcc.LGTM.app.modules.member.domain.Member;
 import swm.hkcc.LGTM.app.modules.member.exception.NotExistMember;
 import swm.hkcc.LGTM.app.modules.member.repository.MemberRepository;
 import swm.hkcc.LGTM.app.modules.member.service.MemberValidator;
 import swm.hkcc.LGTM.app.modules.mission.domain.Mission;
 import swm.hkcc.LGTM.app.modules.mission.dto.CreateMissionRequest;
+import swm.hkcc.LGTM.app.modules.mission.exception.InvalidGithubUrl;
 import swm.hkcc.LGTM.app.modules.mission.repository.MissionRepository;
 import swm.hkcc.LGTM.app.modules.tag.service.TechTagService;
 import swm.hkcc.LGTM.app.modules.tag.service.TechTagValidator;
@@ -38,8 +40,18 @@ public class CreateMissionServiceImpl implements CreateMissionService {
         return mission;
     }
 
-    private void validateRequest(Member writer, List<String> tagList) {
+    private void validateRequest(Member writer, CreateMissionRequest request) {
         memberValidator.validateSenior(writer);
-        techTagValidator.validateTagList(tagList);
+        techTagValidator.validateTagList(request.getTagList());
+        validateGithubUrl(request.getMissionRepositoryUrl());
+    }
+
+    private void validateGithubUrl(String url) {
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            restTemplate.getForObject(url, String.class);
+        } catch (Exception e) {
+            throw new InvalidGithubUrl();
+        }
     }
 }
