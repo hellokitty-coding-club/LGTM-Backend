296102d39407cde3380265557acd35e5dda2c95a
ray-yhc <raycho521@yonsei.ac.kr>
2023-08-22 18:07:24 +0900

    refactor #65: refactor RegistrationService

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/service/RegistrationService.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/service/RegistrationService.java
@@ -34,33 +34,12 @@ public class RegistrationService {
     private final RedisLockRepository redisLockRepository;
 
     public long registerJunior(Member junior, Long missionId) throws InterruptedException {
-        validateJunior(junior);
-        Mission mission = missionRepository.findById(missionId).orElseThrow(NotExistMission::new);
-        if (missionRegistrationRepository.countByMission_MissionIdAndJunior_MemberId(missionId, junior.getMemberId()) > 0) {
-            throw new AlreadyRegisteredMission();
-        }
+        validateMemberPosition(junior, ExpectedPosition.JUNIOR);
+        Mission mission = getValidMission(missionId, junior.getMemberId());
 
-        int countRegisters = missionRegistrationRepository.countByMission_MissionId(missionId);
-
-        validateMission(mission, countRegisters);
-
-        while (!redisLockRepository.lock(mission.getMissionId())) {
-            Thread.sleep(100);
-        }
+        acquireLock(mission.getMissionId());
         try {
-            MissionRegistration missionRegistration = MissionRegistration.builder()
-                    .mission(mission)
-                    .junior(junior)
-                    .status(ProcessStatus.WAITING_FOR_PAYMENT)
-                    .build();
-            missionRegistrationRepository.save(missionRegistration);
-
-            MissionHistory missionHistory = MissionHistory.builder()
-                    .registration(missionRegistration)
-                    .status(ProcessStatus.WAITING_FOR_PAYMENT)
-                    .build();
-            missionHistoryRepository.save(missionHistory);
-            return missionRegistration.getRegistrationId();
+            return processMissionRegistration(junior, mission);
         } finally {
             redisLockRepository.unlock(mission.getMissionId());
         }
@@ -78,15 +57,60 @@ public class RegistrationService {
         }
     }
 
-    private void validateMission(Mission mission, int countRegisters) {
+    private void validateMemberPosition(Member member, ExpectedPosition expectedRole) {
+        if (expectedRole == ExpectedPosition.JUNIOR && member.getJunior() == null) {
+            throw new NotJuniorMember();
+        } else if (expectedRole == ExpectedPosition.SENIOR && member.getSenior() == null) {
+            throw new NotSeniorMember();
+        }
+    }
+
+    private Mission getValidMission(Long missionId, Long memberId) {
+        Mission mission = missionRepository.findById(missionId).orElseThrow(NotExistMission::new);
+        // 이미 등록된 미션인지
+        if (missionRegistrationRepository.countByMission_MissionIdAndJunior_MemberId(mission.getMissionId(), memberId) > 0) {
+            throw new AlreadyRegisteredMission();
+        }
         // 미션 마감일이 지난 경우
         if (mission.getRegistrationDueDate().isBefore(LocalDate.now())) {
             throw new MissRegisterDeadline();
         }
         // 미션 등록인원이 넘치는 경우
+        int countRegisters = missionRegistrationRepository.countByMission_MissionId(mission.getMissionId());
         if (mission.getMaxPeopleNumber() <= countRegisters) {
             throw new FullRegisterMembers();
         }
+        return mission;
     }
 
+    private void acquireLock(Long missionId) throws InterruptedException {
+        int retries = 10;  // 최대 10번 재시도
+        while (retries-- > 0 && !redisLockRepository.lock(missionId)) {
+            Thread.sleep(100);
+        }
+        if (retries <= 0) {
+            throw new RuntimeException("Unable to acquire lock for mission: " + missionId);
+        }
+    }
+
+    private Long processMissionRegistration(Member junior, Mission mission) {
+        MissionRegistration missionRegistration = MissionRegistration.builder()
+                .mission(mission)
+                .junior(junior)
+                .status(ProcessStatus.WAITING_FOR_PAYMENT)
+                .build();
+        missionRegistrationRepository.save(missionRegistration);
+
+        MissionHistory missionHistory = MissionHistory.builder()
+                .registration(missionRegistration)
+                .status(ProcessStatus.WAITING_FOR_PAYMENT)
+                .build();
+        missionHistoryRepository.save(missionHistory);
+        return missionRegistration.getRegistrationId();
+    }
+
+}
+
+enum ExpectedPosition {
+    JUNIOR, SENIOR
 }
