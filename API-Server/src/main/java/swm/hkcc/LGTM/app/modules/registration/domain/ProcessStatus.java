b723b3d73e7a7e864415ac3534c1907d9c3957f3
ray-yhc <raycho521@yonsei.ac.kr>
2023-08-22 15:21:48 +0900

    feat #65: edit ProcessStatus

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/domain/ProcessStatus.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/domain/ProcessStatus.java
@@ -5,15 +5,22 @@ import lombok.Getter;
 @Getter
 public enum ProcessStatus {
 
-    WAITING_FOR_PAYMENT("입금 대기중"),
-    PAYMENT_CONFIRMATION("입금 확인중"),
-    MISSION_PROCEEDING("미션 수행"),
-    CODE_REVIEW("코드 리뷰 작성"),
-    MISSION_FINISHED("미션 완료!");
+    WAITING_FOR_PAYMENT("입금 대기중", false, false, "입금하기"),
+    PAYMENT_CONFIRMATION("입금 확인중", false, false, "입금 확인중"),
+    MISSION_PROCEEDING("미션 수행중", true, false, "미션 수행"),
+    CODE_REVIEW("코드 리뷰 작성", true, true, "코드 리뷰 작성"),
+    MISSION_FINISHED("미션 완료!",  true, true, "미션 완료!"),
+    FEEDBACK_REVIEWED("리뷰 완료!", true, true, "리뷰 완료!");
 
     private final String status;
+    private final boolean isPayed;
+    private final boolean isPullRequestCreated;
+    private final String buttonMessage;
 
-    PersonalStatus(String status) {
+    ProcessStatus(String status, boolean isPayed, boolean isPullRequestCreated, String buttonMessage) {
         this.status = status;
+        this.isPayed = isPayed;
+        this.isPullRequestCreated = isPullRequestCreated;
+        this.buttonMessage = buttonMessage;
     }
 }
