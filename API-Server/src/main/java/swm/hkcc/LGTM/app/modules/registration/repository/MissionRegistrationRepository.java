2489e171eb59f36aaa7af0b1e3ee5ba0bd69473f
ray-yhc <raycho521@yonsei.ac.kr>
2023-08-22 18:19:24 +0900

    feat #66: add custom repositories

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/repository/MissionRegistrationRepository.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/repository/MissionRegistrationRepository.java
@@ -4,7 +4,12 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import swm.hkcc.LGTM.app.modules.registration.domain.MissionRegistration;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MissionRegistrationRepository extends JpaRepository<MissionRegistration, Long> {
     @Cacheable(value = "mission_participant_count", key = "#missionId")
     int countByMission_MissionId(Long missionId);
+
+    int countByMission_MissionIdAndJunior_MemberId(Long missionId, Long juniorId);
 }
