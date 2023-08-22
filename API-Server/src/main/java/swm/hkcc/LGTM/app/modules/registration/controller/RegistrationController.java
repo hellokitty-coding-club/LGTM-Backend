446f0bacc620d51774c90e0b0acfd75d5d6178e5
ray-yhc <raycho521@yonsei.ac.kr>
2023-08-22 15:24:52 +0900

    feat #65: add controllers for registeration

---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/controller/RegistrationController.java
@@ -0,0 +1,36 @@
+package swm.hkcc.LGTM.app.modules.registration.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import swm.hkcc.LGTM.app.global.dto.ApiDataResponse;
+import swm.hkcc.LGTM.app.modules.member.domain.Member;
+import swm.hkcc.LGTM.app.modules.member.domain.custom.CustomUserDetails;
+import swm.hkcc.LGTM.app.modules.registration.service.RegistrationService;
+
+@RestController
+@RequestMapping("/v1/mission/{missionId}")
+@RequiredArgsConstructor
+public class RegistrationController {
+    private final RegistrationService registrationService;
+
+    /**
+     * 주니어가 미션에 참가한다
+     *
+     * @param customUserDetails
+     * @param missionId
+     */
+    @PostMapping
+    public ApiDataResponse<Boolean> enrollMission(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @PathVariable Long missionId
+    ) {
+        Member junior = customUserDetails.getMember();
+        registrationService.registerJunior(junior, missionId);
+        return ApiDataResponse.of(true);
+    }
+
+}
