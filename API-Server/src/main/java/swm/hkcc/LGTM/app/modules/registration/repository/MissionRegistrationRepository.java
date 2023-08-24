91d55b5c6c038453f0e657b657fd6eccbe71be0d
ray-yhc <raycho521@yonsei.ac.kr>
2023-08-24 21:06:49 +0900

    chore #65: add custom repository

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/repository/MissionRegistrationRepository.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/repository/MissionRegistrationRepository.java
@@ -7,8 +7,8 @@ import swm.hkcc.LGTM.app.modules.registration.domain.MissionRegistration;
 import java.util.List;
 import java.util.Optional;
 
-public interface MissionRegistrationRepository extends JpaRepository<MissionRegistration, Long> {
-    @Cacheable(value = "mission_participant_count", key = "#missionId")
+public interface MissionRegistrationRepository extends JpaRepository<MissionRegistration, Long>, MissionRegistrationCustomRepository {
+    @Cacheable(value = "mission_participant_count", key = "#p0")
     int countByMission_MissionId(Long missionId);
 
     int countByMission_MissionIdAndJunior_MemberId(Long missionId, Long juniorId);
