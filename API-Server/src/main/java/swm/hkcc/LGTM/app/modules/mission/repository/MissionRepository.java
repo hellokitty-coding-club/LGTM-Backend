ee92c24ddbf257115c8c01fe5f37ba3b771aff91
great-park <cksgh1735@gmail.com>
2023-09-06 14:53:52 +0900

    feat #78: 시니어의 진행한 미션 조회

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/repository/MissionRepository.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/repository/MissionRepository.java
@@ -1,11 +1,12 @@
 package swm.hkcc.LGTM.app.modules.mission.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import swm.hkcc.LGTM.app.modules.mission.domain.Mission;
 
+import java.util.List;
+
 public interface MissionRepository extends JpaRepository<Mission, Long>, MissionCustomRepository {
-//    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END FROM Mission m WHERE m.missionId = :missionId AND m.writer.memberId = :memberId")
-//    boolean existsByMissionIdAndMemberId(Long missionId, Long memberId);
     boolean existsByMissionIdAndWriter_MemberId(Long missionId, Long memberId);
+
+    List<Mission> findAllByWriter_MemberId(Long memberId);
 }
