b1e3519f98b7b3cf463743ef8bca0a232ad4676e
great-park <cksgh1735@gmail.com>
2023-09-06 16:40:37 +0900

    test #78: 사용자 프로필 조회 api 테스트 작성

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/member/repository/JuniorRepository.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/member/repository/JuniorRepository.java
@@ -3,8 +3,12 @@ package swm.hkcc.LGTM.app.modules.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import swm.hkcc.LGTM.app.modules.member.domain.Junior;
+import swm.hkcc.LGTM.app.modules.member.domain.Senior;
+
+import java.util.Optional;
 
 @Repository
 public interface JuniorRepository extends JpaRepository<Junior, Long> {
     boolean existsByMember_MemberId(Long memberId);
+    Optional<Junior> findByMember_MemberId(Long memberId);
 }
