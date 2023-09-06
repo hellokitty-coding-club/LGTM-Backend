780ade28a0b3449b1b4faaeabe2beedbac7978df
great-park <cksgh1735@gmail.com>
2023-09-06 14:55:01 +0900

    feat #78: member의 기술태그 조회, N+1 문제 방지를 위해 Fetch join 사용

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/tag/repository/TechTagPerMemberRepository.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/tag/repository/TechTagPerMemberRepository.java
@@ -1,8 +1,11 @@
 package swm.hkcc.LGTM.app.modules.tag.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import swm.hkcc.LGTM.app.modules.member.domain.Member;
+import swm.hkcc.LGTM.app.modules.tag.domain.TechTag;
 import swm.hkcc.LGTM.app.modules.tag.domain.TechTagPerMember;
 
 import java.util.List;
@@ -10,4 +13,7 @@ import java.util.List;
 @Repository
 public interface TechTagPerMemberRepository extends JpaRepository<TechTagPerMember, Long> {
     List<TechTagPerMember> findByMember(Member member);
+
+    @Query("SELECT t FROM TechTagPerMember t JOIN FETCH t.techTag WHERE t.member.memberId = :memberId")
+    List<TechTagPerMember> findWithTechTagByMemberId(@Param("memberId") Long memberId);
 }
