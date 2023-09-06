7682417dd9fa34ace47431674d4bdc75a4e44a95
great-park <cksgh1735@gmail.com>
2023-09-06 14:55:29 +0900

    feat #78: 최종 dto로 변환하기 위한 dto mapper

---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/member/domain/mapper/MemberMapper.java
@@ -0,0 +1,25 @@
+package swm.hkcc.LGTM.app.modules.member.domain.mapper;
+
+import swm.hkcc.LGTM.app.modules.member.domain.Member;
+import swm.hkcc.LGTM.app.modules.member.dto.MemberDetailInfo;
+import swm.hkcc.LGTM.app.modules.member.dto.MemberDetailProfile;
+import swm.hkcc.LGTM.app.modules.mission.dto.MissionDto;
+import swm.hkcc.LGTM.app.modules.tag.domain.TechTag;
+
+import java.util.List;
+
+public class MemberMapper {
+
+    public static MemberDetailProfile toMemberDetailProfile(Member member, MemberDetailInfo memberDetailInfo, List<TechTag> techTagList, List<MissionDto> missionDtos) {
+        return MemberDetailProfile.builder()
+                .memberId(member.getMemberId())
+                .githubId(member.getGithubId())
+                .nickName(member.getNickName())
+                .profileImageUrl(member.getProfileImageUrl())
+                .techTagList(techTagList)
+                .isAgreeWithEventInfo(member.isAgreeWithEventInfo())
+                .memberDetailInfo(memberDetailInfo)
+                .memberMissionHistory(missionDtos)
+                .build();
+    }
+}
