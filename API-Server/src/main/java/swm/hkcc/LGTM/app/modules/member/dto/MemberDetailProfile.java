6ff31d3c4863ca8c3548bcdbe52d68196d0e925b
great-park <cksgh1735@gmail.com>
2023-09-06 14:51:50 +0900

    feat #78: member의 프로필 조회를 위한 DTO 생성

---
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/member/dto/MemberDetailProfile.java
@@ -0,0 +1,21 @@
+package swm.hkcc.LGTM.app.modules.member.dto;
+
+import lombok.*;
+import swm.hkcc.LGTM.app.modules.mission.dto.MissionDto;
+import swm.hkcc.LGTM.app.modules.tag.domain.TechTag;
+
+import java.util.List;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class MemberDetailProfile {
+    private Long memberId;
+    private String githubId;
+    private String nickName;
+    private String profileImageUrl;
+    private List<TechTag> techTagList;
+    private boolean isAgreeWithEventInfo;
+    private MemberDetailInfo memberDetailInfo;
+    private List<MissionDto> memberMissionHistory;
+}
