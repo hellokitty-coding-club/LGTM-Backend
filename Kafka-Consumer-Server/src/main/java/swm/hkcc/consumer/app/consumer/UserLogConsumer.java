6176bb9696402485c595acae5beed1c05f4bb4f0
ray-yhc <raycho521@yonsei.ac.kr>
2023-09-07 13:01:19 +0900

    feat #33: edit consumer

--- Kafka-Consumer-Server/src/main/java/swm/hkcc/consumer/app/consumer/UserLogConsumer.java
+++ Kafka-Consumer-Server/src/main/java/swm/hkcc/consumer/app/consumer/UserLogConsumer.java
@@ -6,9 +6,7 @@ import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
-import swm.hkcc.consumer.app.dto.CommonMessage;
-import swm.hkcc.consumer.app.dto.TimeIntervalLogMessage;
-import swm.hkcc.consumer.app.dto.TimestampLogMessage;
+import swm.hkcc.consumer.app.dto.LogMessage;
 
 @Service
 @Slf4j
@@ -16,12 +14,11 @@ public class UserLogConsumer {
 
     @KafkaListener(topics = "${spring.kafka.topic.user-log}", groupId = "${spring.kafka.group-id.user-log}")
     public void consume(
-            @Payload CommonMessage message,
-            @Header(KafkaHeaders.RECEIVED_KEY) String key,
+            @Payload LogMessage message,
             @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
             @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
             @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long ts
     ) {
-        log.info(String.format("Json message recieved -> %s", message.getMessageByKey(key).toString()));
+        log.info(String.format("Json message recieved -> %s", message.toString()));
     }
 }
