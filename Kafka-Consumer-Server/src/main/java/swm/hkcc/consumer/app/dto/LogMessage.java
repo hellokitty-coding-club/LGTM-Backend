a0e231bc141e866b2660e5e7dcdbbc6f516d4553
ray-yhc <raycho521@yonsei.ac.kr>
2023-09-07 13:00:44 +0900

    feat #33: add log message dto

---
+++ Kafka-Consumer-Server/src/main/java/swm/hkcc/consumer/app/dto/LogMessage.java
@@ -0,0 +1,17 @@
+package swm.hkcc.consumer.app.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.Map;
+
+@Data
+@Getter @ToString
+public class LogMessage {
+    private String eventLogName;
+    private String screenName;
+    private Integer logVersion;
+
+    private Map<String, Object> logData;
+}
