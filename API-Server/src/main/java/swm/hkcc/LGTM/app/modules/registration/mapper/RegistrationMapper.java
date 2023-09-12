53a7c85d172b13d97e410929d45de7fccd0045a2
ray-yhc <raycho521@yonsei.ac.kr>
2023-09-12 16:23:35 +0900

    docs #67: add mapper method

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/mapper/RegistrationMapper.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/mapper/RegistrationMapper.java
@@ -1,8 +1,12 @@
 package swm.hkcc.LGTM.app.modules.registration.mapper;
 
 import swm.hkcc.LGTM.app.modules.mission.domain.Mission;
+import swm.hkcc.LGTM.app.modules.registration.domain.ProcessStatus;
 import swm.hkcc.LGTM.app.modules.registration.dto.MemberRegisterSimpleInfo;
+import swm.hkcc.LGTM.app.modules.registration.dto.MissionHistoryInfo;
 import swm.hkcc.LGTM.app.modules.registration.dto.RegistrationSeniorResponse;
+import swm.hkcc.LGTM.app.modules.registration.dto.registrationJuniorResponse.JuniorAdditionalInfo;
+import swm.hkcc.LGTM.app.modules.registration.dto.registrationJuniorResponse.RegistrationJuniorResponse;
 import swm.hkcc.LGTM.app.modules.tag.domain.TechTag;
 
 import java.util.List;
@@ -20,5 +24,23 @@ public class RegistrationMapper {
                 .techTagList(techTagList)
                 .memberInfoList(memberInfoList)
                 .build();
+    }
+
+    public static RegistrationJuniorResponse toRegistrationJuniorResponse(
+            Mission mission,
+            List<TechTag> techTagList,
+            List<MissionHistoryInfo> missinHistory,
+            ProcessStatus status,
+            JuniorAdditionalInfo additionalInfo
+    ) {
+        RegistrationJuniorResponse response = additionalInfo.createResponse();
+
+        response.setMissionName(mission.getTitle());
+        response.setTechTagList(techTagList);
+        response.setProcessStatus(status);
+        response.setMissionHistory(missinHistory);
+        response.setButtonTitle(status.getJuniorBottomTitle());
+
+        return response;
     }
 }
