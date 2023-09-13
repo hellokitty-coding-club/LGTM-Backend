ce6a376d5d20aeca758d9e5a7c8fecea07af1a4e
ray-yhc <raycho521@yonsei.ac.kr>
2023-09-13 19:39:22 +0900

    feat #96: add exceptions

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/exception/InvalidProcessStatus.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/exception/InvalidProcessStatus.java
@@ -2,9 +2,16 @@ package swm.hkcc.LGTM.app.modules.registration.exception;
 
 import swm.hkcc.LGTM.app.global.constant.ResponseCode;
 import swm.hkcc.LGTM.app.global.exception.GeneralException;
+import swm.hkcc.LGTM.app.modules.registration.domain.ProcessStatus;
 
 public class InvalidProcessStatus extends GeneralException {
     public InvalidProcessStatus() {
         super(ResponseCode.INVALID_PROCESS_STATUS);
+    }
+    public InvalidProcessStatus(ProcessStatus currentStatus) {
+        super(
+                ResponseCode.INVALID_PROCESS_STATUS,
+                ResponseCode.INVALID_PROCESS_STATUS.getMessage() + "현재 상태: " + currentStatus
+        );
     }
 }
