3a82e11606c883c3dbc190c92376e48a9f0635bc
ray-yhc <raycho521@yonsei.ac.kr>
2023-09-18 21:14:01 +0900

    refactor #96: refactor github url validator

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/CreateMissionServiceImpl.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/service/CreateMissionServiceImpl.java
@@ -4,14 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 import swm.hkcc.LGTM.app.modules.member.domain.Member;
 import swm.hkcc.LGTM.app.modules.member.service.MemberValidator;
 import swm.hkcc.LGTM.app.modules.mission.domain.Mission;
 import swm.hkcc.LGTM.app.modules.mission.dto.CreateMissionRequest;
-import swm.hkcc.LGTM.app.modules.mission.exception.InvalidGithubUrl;
 import swm.hkcc.LGTM.app.modules.mission.repository.MissionRepository;
+import swm.hkcc.LGTM.app.modules.mission.utils.GithubUrlValidator;
 import swm.hkcc.LGTM.app.modules.tag.service.TechTagService;
 import swm.hkcc.LGTM.app.modules.tag.service.TechTagValidator;
 
@@ -40,30 +38,6 @@ public class CreateMissionServiceImpl implements CreateMissionService {
     private void validateRequest(Member writer, CreateMissionRequest request) {
         memberValidator.validateSenior(writer);
         techTagValidator.validateTagList(request.getTagList());
-        validateGithubUrl(request.getMissionRepositoryUrl());
+        GithubUrlValidator.validateGithubRepositoryUrl(request.getMissionRepositoryUrl());
     }
-
-    public void validateGithubUrl(String url) {
-        if (!isValidGithubUrl(url) || !existsGithubRepo(url)) {
-            throw new InvalidGithubUrl();
-        }
-    }
-
-    public boolean isValidGithubUrl(String url) {
-        String regex = "^https://github\\.com/[^/]+/[^/]+/?$";
-        return url.matches(regex);
-    }
-
-    public boolean existsGithubRepo(String url) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        try {
-            restTemplate.headForHeaders(url);  // HEAD 요청으로 해당 URL의 헤더만 받아옵니다.
-            return true;
-        } catch (HttpClientErrorException e) {
-            // 404 등의 에러 코드가 반환될 경우 해당 URL은 존재하지 않습니다.
-            return false;
-        }
-    }
-
 }
