77875440179cecd3081af84ae6d559cc1526eca6
ray-yhc <raycho521@yonsei.ac.kr>
2023-09-26 20:36:55 +0900

    feat #33: add toUserLog

--- Kafka-Consumer-Server/src/main/java/swm/hkcc/consumer/app/dto/LogMessage.java
+++ Kafka-Consumer-Server/src/main/java/swm/hkcc/consumer/app/dto/LogMessage.java
@@ -3,6 +3,7 @@ package swm.hkcc.consumer.app.dto;
 import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
+import swm.hkcc.consumer.app.domain.UserLog;
 
 import java.util.Map;
 
@@ -24,6 +25,18 @@ public class LogMessage {
                 .logData(logData)
                 .topic(topic)
                 .partition(partition)
+                .receivedTimeStamp(receivedTimeStamp)
+                .build();
+    }
+
+    public UserLog toUserLog(String topic, int partition, long receivedTimeStamp) {
+        return UserLog.builder()
+                .eventLogName(eventLogName)
+                .screenName(screenName)
+                .logVersion(logVersion)
+                .logData(logData)
+                .topic(topic)
+                .partitionNumber(partition)
                 .receivedTimeStamp(receivedTimeStamp)
                 .build();
     }
