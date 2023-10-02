801fdf4fff4955fa7eba9c9efd027576a7f7016c
great-park <cksgh1735@gmail.com>
2023-10-02 14:58:23 +0900

    fix #101: change root project name

--- lgtm/src/main/java/swm/hkcc/LGTM/app/modules/auth/constants/TokenType.java
+++
@@ -1,6 +0,0 @@
-package swm.hkcc.LGTM.app.modules.auth.constants;
-
-public enum TokenType {
-    ACCESS_TOKEN,
-    REFRESH_TOKEN,
-}
--- lgtm/src/main/java/swm/hkcc/LGTM/app/modules/member/domain/CertificationStatus.java
+++
@@ -1,5 +0,0 @@
-package swm.hkcc.LGTM.app.modules.member.domain;
-
-public enum CertificationStatus {
-    UNCERTIFIED, PENDING, CERTIFIED
-}
