68058e0b64bb79c57a9bb6540ad4e77b88fd5301
great-park <cksgh1735@gmail.com>
2023-10-06 13:45:48 +0900

    feat #101: key 값은 String 값을 사용해야하므로 변환

--- Chatting-Server/src/main/java/swm/hkcc/chat/app/modules/chat/service/ChatConsumer.java
+++ Chatting-Server/src/main/java/swm/hkcc/chat/app/modules/chat/service/ChatConsumer.java
@@ -8,33 +8,35 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
-import swm.hkcc.chat.app.modules.chat.model.ChatDto;
+import swm.hkcc.chat.app.modules.chat.domain.ChatMessage;
+import swm.hkcc.chat.app.modules.chat.repository.ChatRepository;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ChatConsumer {
     private final SimpMessageSendingOperations template;
+    private final ChatRepository chatRepository;
 
-    @KafkaListener(topics = "${spring.kafka.topic.chat-message}", groupId = "${spring.kafka.group-id.chat-message-repository}")
+    @KafkaListener(topics = "${spring.kafka.topic.chat-message}", groupId = "fsdf")
     public void consume(
-            @Payload ChatDto chat,
+            @Payload ChatMessage chat,
             @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
             @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
             @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long ts
     ) {
-        template.convertAndSend("/sub/chatroom/detail/" + chat.getRoomId(), chat);
+        String str_roomId = String.valueOf(chat.getRoomId());
+        template.convertAndSend("/sub/chatroom/detail/" + str_roomId, chat);
     }
 
 
     @KafkaListener(topics = "${spring.kafka.topic.chat-message}", groupId = "${spring.kafka.group-id.chat-message-repository}")
     public void persist(
-            @Payload ChatDto chat,
+            @Payload ChatMessage chat,
             @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
             @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
             @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long ts
     ) {
-        // mongoDB에 저장
+        chatRepository.save(chat);
     }
-
 }
--- Chatting-Server/src/main/java/swm/hkcc/chat/app/modules/chat/service/ChatProducer.java
+++ Chatting-Server/src/main/java/swm/hkcc/chat/app/modules/chat/service/ChatProducer.java
@@ -8,22 +8,23 @@ import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
-import swm.hkcc.chat.app.modules.chat.model.ChatDto;
+import swm.hkcc.chat.app.modules.chat.domain.ChatMessage;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ChatProducer {
 
-    private final KafkaTemplate<String, ChatDto> kafkaTemplate;
+    private final KafkaTemplate<String, ChatMessage> kafkaTemplate;
 
     @Value("${spring.kafka.topic.chat-message}")
     private String TOPIC_NAME;
 
-    public void sendChatMessage(ChatDto chatDto) {
-        Message<ChatDto> message = MessageBuilder
-                .withPayload(chatDto)
-                .setHeader(KafkaHeaders.KEY, chatDto.getRoomId())
+    public void sendChatMessage(ChatMessage chatMessage) {
+        String str_roomId = String.valueOf(chatMessage.getRoomId());
+        Message<ChatMessage> message = MessageBuilder
+                .withPayload(chatMessage)
+                .setHeader(KafkaHeaders.KEY, str_roomId)
                 .setHeader(KafkaHeaders.TOPIC, TOPIC_NAME)
                 .build();
 
