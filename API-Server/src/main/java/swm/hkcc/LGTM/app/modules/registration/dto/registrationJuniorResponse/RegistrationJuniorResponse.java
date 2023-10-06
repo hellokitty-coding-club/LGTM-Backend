7d264b18c3104a8a1b63432bae47d7ed252403d7
ray-yhc <raycho521@yonsei.ac.kr>
2023-10-07 00:26:30 +0900

    feat #67: edit response dto structure

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/dto/registrationJuniorResponse/RegistrationJuniorResponse.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/dto/registrationJuniorResponse/RegistrationJuniorResponse.java
@@ -7,7 +7,9 @@ import swm.hkcc.LGTM.app.modules.registration.domain.ProcessStatus;
 import swm.hkcc.LGTM.app.modules.registration.dto.MissionHistoryInfo;
 import swm.hkcc.LGTM.app.modules.tag.domain.TechTag;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SuperBuilder
 @Data
@@ -16,6 +18,13 @@ public class RegistrationJuniorResponse {
     protected String missionName;
     protected List<TechTag> techTagList;
     protected ProcessStatus processStatus;
-    protected List<MissionHistoryInfo> missionHistory;
+    protected Map<ProcessStatus,String> missionHistory;
     protected String buttonTitle;
+
+    public void setMissionHistory(List<MissionHistoryInfo> missionHistory) {
+        this.missionHistory = new HashMap<>();
+        for (MissionHistoryInfo history : missionHistory) {
+            this.missionHistory.put(history.getStatus(),history.getDateTime());
+        }
+    }
 }
