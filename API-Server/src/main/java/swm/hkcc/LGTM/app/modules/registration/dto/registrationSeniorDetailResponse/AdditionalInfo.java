a14ec4dd325ebc62649a420d8e9309798945038d
ray-yhc <raycho521@yonsei.ac.kr>
2023-10-10 23:13:46 +0900

    Revert "refactor #67: fix additionalInfo"
    This reverts commit 34bdd6173594e1f1140278468a3664692dc71bfb.

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/dto/registrationSeniorDetailResponse/AdditionalInfo.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/dto/registrationSeniorDetailResponse/AdditionalInfo.java
@@ -1,10 +1,7 @@
 package swm.hkcc.LGTM.app.modules.registration.dto.registrationSeniorDetailResponse;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class AdditionalInfo {
-    public Map<String, Object> getAdditionalInfo() {
-        return new HashMap<>();
+    public RegistrationSeniorDetailResponse createResponse() {
+        return new RegistrationSeniorDetailResponse();
     }
 }
