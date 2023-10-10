990ef77ff289cd48c111fa98e0857aadbc813c64
ray-yhc <raycho521@yonsei.ac.kr>
2023-10-10 15:09:55 +0900

    feat #107: add fcm connection

---
+++ API-Server/src/main/java/swm/hkcc/LGTM/config/FcmConfig.java
@@ -0,0 +1,39 @@
+package swm.hkcc.LGTM.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Configuration
+public class FcmConfig {
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        ClassPathResource resource = new ClassPathResource("firebase/lgtm-aeb7a-firebase-adminsdk-sx5gi-c8b21c8e56.json");
+        InputStream refreshToken = resource.getInputStream();
+
+        FirebaseApp firebaseApp = null;
+        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
+
+        if (firebaseAppList != null && !firebaseAppList.isEmpty()) {
+            for (FirebaseApp app : firebaseAppList) {
+                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+                    firebaseApp = app;
+                }
+            }
+        } else {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                    .build();
+            firebaseApp = FirebaseApp.initializeApp(options);
+        }
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+}
