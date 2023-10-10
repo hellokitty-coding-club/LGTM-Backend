9fa8298df59489723dcc52e7110e23a240214cd2
ray-yhc <raycho521@yonsei.ac.kr>
2023-10-10 21:56:45 +0900

    refactor #67: fix additionalInfo

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/dto/registrationJuniorResponse/JuniorAdditionalFeedbackInfo.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/dto/registrationJuniorResponse/JuniorAdditionalFeedbackInfo.java
@@ -3,15 +3,17 @@ package swm.hkcc.LGTM.app.modules.registration.dto.registrationJuniorResponse;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 
+import java.util.Map;
+
 @Data
 @SuperBuilder
 public class JuniorAdditionalFeedbackInfo extends JuniorAdditionalInfo {
     private Long reviewId;
 
-
-    public RegistrationJuniorResponse createResponse() {
-        RegistrationJuniorFeedbackResponse response = new RegistrationJuniorFeedbackResponse();
-        response.setReviewId(this.getReviewId());
-        return response;
+    @Override
+    public Map<String, Object> getAdditionalInfo() {
+        Map<String, Object> additionalInfo = super.getAdditionalInfo();
+        additionalInfo.put("feedbackId", reviewId);
+        return additionalInfo;
     }
 }
