9ca9f4c7f0e3b80c75276f3a3e1497cf844e03a0
ray-yhc <raycho521@yonsei.ac.kr>
2023-10-19 17:36:36 +0900

    feat: edit locale

--- API-Server/src/main/java/swm/hkcc/LGTM/LgtmApplication.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/LgtmApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+import java.util.Locale;
+
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class LgtmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LgtmApplication.class, args);
+		Locale.setDefault(Locale.KOREA);
 	}
 
 }
