8faf2fe2ee18a0e86a9ee63a38ecdce515da6ad4
ray-yhc <raycho521@yonsei.ac.kr>
2023-10-23 18:17:14 +0900

    fix #107: edit RegistrationController

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/controller/RegistrationController.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/registration/controller/RegistrationController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.*;
 import swm.hkcc.LGTM.app.global.dto.ApiDataResponse;
 import swm.hkcc.LGTM.app.modules.member.domain.Member;
 import swm.hkcc.LGTM.app.modules.member.domain.custom.CustomUserDetails;
+import swm.hkcc.LGTM.app.modules.member.service.MemberService;
+import swm.hkcc.LGTM.app.modules.mission.domain.Mission;
+import swm.hkcc.LGTM.app.modules.mission.service.MissionService;
 import swm.hkcc.LGTM.app.modules.registration.dto.MissionHistoryInfo;
 import swm.hkcc.LGTM.app.modules.registration.dto.PullRequestRegisterRequest;
 import swm.hkcc.LGTM.app.modules.registration.dto.RegistrationSeniorResponse;
@@ -19,6 +22,8 @@ import swm.hkcc.LGTM.app.modules.registration.service.RegistrationService;
 @RequiredArgsConstructor
 public class RegistrationController {
     private final RegistrationService registrationService;
+    private final MissionService missionService;
+    private final MemberService memberService;
 
     /**
      * 주니어가 미션에 참가한다
@@ -32,7 +37,8 @@ public class RegistrationController {
             @PathVariable Long missionId
     ) throws InterruptedException {
         Member junior = customUserDetails.getMember();
-        registrationService.registerJunior(junior, missionId);
+        Mission mission = missionService.getMission(missionId);
+        registrationService.registerJunior(junior, mission);
         return ApiDataResponse.of(true);
     }
 
@@ -48,8 +54,8 @@ public class RegistrationController {
             @PathVariable Long missionId
     ) {
         Member junior = customUserDetails.getMember();
-
-        return ApiDataResponse.of(registrationService.getJuniorEnrollInfo(junior, missionId));
+        Mission mission = missionService.getMission(missionId);
+        return ApiDataResponse.of(registrationService.getJuniorEnrollInfo(junior, mission));
     }
 
     @GetMapping("/senior")
@@ -58,7 +64,8 @@ public class RegistrationController {
             @PathVariable Long missionId
     ) {
         Member senior = customUserDetails.getMember();
-        return ApiDataResponse.of(registrationService.getSeniorEnrollInfo(senior, missionId));
+        Mission mission = missionService.getMission(missionId);
+        return ApiDataResponse.of(registrationService.getSeniorEnrollInfo(senior, mission));
     }
 
     @PostMapping("/confirm/{juniorId}")
@@ -68,7 +75,8 @@ public class RegistrationController {
             @PathVariable Long juniorId
     ) {
         Member senior = customUserDetails.getMember();
-        return ApiDataResponse.of(registrationService.confirmPayment(senior, missionId, juniorId));
+        Mission mission = missionService.getMission(missionId);
+        return ApiDataResponse.of(registrationService.confirmPayment(senior, mission, juniorId));
     }
 
     @PostMapping("/review/{juniorId}")
@@ -78,7 +86,8 @@ public class RegistrationController {
             @PathVariable Long juniorId
     ) {
         Member senior = customUserDetails.getMember();
-        return ApiDataResponse.of(registrationService.completeReview(senior, missionId, juniorId));
+        Mission mission = missionService.getMission(missionId);
+        return ApiDataResponse.of(registrationService.completeReview(senior, mission, juniorId));
     }
 
     @PostMapping("/payment")
@@ -87,7 +96,8 @@ public class RegistrationController {
             @PathVariable Long missionId
     ) {
         Member junior = customUserDetails.getMember();
-        return ApiDataResponse.of(registrationService.registerPayment(junior, missionId));
+        Mission mission = missionService.getMission(missionId);
+        return ApiDataResponse.of(registrationService.registerPayment(junior, mission));
     }
 
     @PostMapping("/pr")
@@ -97,7 +107,8 @@ public class RegistrationController {
             @RequestBody @Validated PullRequestRegisterRequest prRequest
     ) {
         Member junior = customUserDetails.getMember();
-        return ApiDataResponse.of(registrationService.registerPullRequest(junior, missionId, prRequest.getGithubPrUrl()));
+        Mission mission = missionService.getMission(missionId);
+        return ApiDataResponse.of(registrationService.registerPullRequest(junior, mission, prRequest.getGithubPrUrl()));
     }
 
     @GetMapping("/senior/{juniorId}")
@@ -107,7 +118,9 @@ public class RegistrationController {
             @PathVariable Long juniorId
     ) {
         Member senior = customUserDetails.getMember();
-        return ApiDataResponse.of(registrationService.getSeniorEnrollDetail(senior, missionId, juniorId));
+        Mission mission = missionService.getMission(missionId);
+        Member junior = memberService.getMember(juniorId);
+        return ApiDataResponse.of(registrationService.getSeniorEnrollDetail(senior, mission, junior));
     }
 
 }
