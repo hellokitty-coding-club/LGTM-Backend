7f3f08373927f262b2726ed02055941be380330f
ray-yhc <raycho521@yonsei.ac.kr>
2023-10-25 23:54:03 +0900

    feat #107: edit IntroController

--- API-Server/src/main/java/swm/hkcc/LGTM/app/global/api/IntroController.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/global/api/IntroController.java
@@ -1,15 +1,16 @@
 package swm.hkcc.LGTM.app.global.api;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import swm.hkcc.LGTM.app.global.api.dto.IntroResponse;
+import swm.hkcc.LGTM.app.global.api.dto.PushTestRequest;
+import swm.hkcc.LGTM.app.global.constant.ResponseCode;
 import swm.hkcc.LGTM.app.global.dto.ApiDataResponse;
+import swm.hkcc.LGTM.app.global.exception.GeneralException;
 import swm.hkcc.LGTM.app.global.notification.service.NotificationService;
 
-import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/intro")
@@ -23,17 +24,20 @@ public class IntroController {
         return ApiDataResponse.of(new IntroResponse(100, 100));
     }
 
-    @GetMapping("/push")
+    @PostMapping("/push")
     public ApiDataResponse<Long> pushTest(
-            @RequestParam Long targetMemberId
+            @RequestParam(required = false) Optional<Long> targetMemberId,
+            @RequestParam(required = false) Optional<Boolean> isBroadcast,
+            @RequestBody @Validated PushTestRequest request
     ) {
-        Map<String, String> data = Map.of(
-                "title", "테스트 푸시 제목",
-                "body", "테스트 푸시 본문",
-                "isFromPush", "true"
-        );
-        notificationService.sendNotification(targetMemberId, data);
-        // 임시로 100,100 넣어둠
-        return ApiDataResponse.of(targetMemberId);
+
+        if (isBroadcast.orElse(false)) {
+            notificationService.broadcast(request.getData());
+            return ApiDataResponse.of(0L);
+        }
+        return targetMemberId.map(id -> {
+            notificationService.sendNotification(id, request.getData());
+            return ApiDataResponse.of(id);
+        }).orElseThrow(() -> new GeneralException(ResponseCode.BAD_REQUEST, "targetMemberId 또는 isBroadcast 둘 중 하나는 필수입니다."));
     }
 }
