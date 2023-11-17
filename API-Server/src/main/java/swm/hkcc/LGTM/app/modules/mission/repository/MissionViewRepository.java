639392ee1de5043da3e39e1e512c1ba3612202f2
ray-yhc <raycho521@yonsei.ac.kr>
2023-11-17 20:57:00 +0900

    feat #139: add new repository

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/repository/MissionViewRepository.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/repository/MissionViewRepository.java
@@ -4,7 +4,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import swm.hkcc.LGTM.app.modules.mission.domain.MissionView;
 
-public interface MissionViewRepository extends JpaRepository<MissionView, Long> {
+public interface MissionViewRepository extends JpaRepository<MissionView, Long>, MissionViewCustomRepository {
     @Cacheable(value = "mission_view_count", key = "#p0")
     int countByMission_MissionId(Long missionId);
 }
