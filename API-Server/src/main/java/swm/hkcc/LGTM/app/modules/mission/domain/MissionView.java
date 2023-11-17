525875092c6dc5e6d2a65cd569e614043ebc547f
ray-yhc <raycho521@yonsei.ac.kr>
2023-11-17 20:58:49 +0900

    feat #139: edit MissionView

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/domain/MissionView.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/mission/domain/MissionView.java
@@ -5,12 +5,14 @@ import lombok.*;
 import swm.hkcc.LGTM.app.global.entity.BaseEntity;
 import swm.hkcc.LGTM.app.modules.member.domain.Member;
 
+import java.io.Serializable;
+
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class MissionView extends BaseEntity {
+public class MissionView extends BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "mission_view_id")
@@ -23,4 +25,11 @@ public class MissionView extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "mission_id")
     private Mission mission;
+
+    public static MissionView from(Mission mission, Member member) {
+        return MissionView.builder()
+                .mission(mission)
+                .viewer(member)
+                .build();
+    }
 }
