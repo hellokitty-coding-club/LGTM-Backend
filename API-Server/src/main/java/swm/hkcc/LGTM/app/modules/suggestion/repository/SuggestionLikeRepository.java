78e8b7a8065992430e895aa09e05069a92d2ea5c
Yoonho Cho (Ray) <raycho521@yonsei.ac.kr>
2024-01-08 18:25:01 +0900

    test #152: add method in SuggestionLikeRepository

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/suggestion/repository/SuggestionLikeRepository.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/suggestion/repository/SuggestionLikeRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import swm.hkcc.LGTM.app.modules.suggestion.domain.SuggestionLike;
 
 public interface SuggestionLikeRepository extends JpaRepository<SuggestionLike, Long> {
+    Integer countBySuggestion_SuggestionId(Long suggestionId);
+    Boolean existsBySuggestion_SuggestionIdAndMember_MemberId(Long suggestionId, Long memberId);
 }
