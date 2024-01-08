e472fcac75f1d13e8f6987a8567d53d6e7ca74e3
Yoonho Cho (Ray) <raycho521@yonsei.ac.kr>
2024-01-08 18:25:49 +0900

    feat #152: add service

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/suggestion/service/SuggestionService.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/suggestion/service/SuggestionService.java
@@ -2,14 +2,20 @@ package swm.hkcc.LGTM.app.modules.suggestion.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import swm.hkcc.LGTM.app.modules.member.domain.Member;
 import swm.hkcc.LGTM.app.modules.suggestion.domain.Suggestion;
 import swm.hkcc.LGTM.app.modules.suggestion.dto.CreateSuggestionRequest;
 import swm.hkcc.LGTM.app.modules.suggestion.dto.CreateSuggestionResponse;
+import swm.hkcc.LGTM.app.modules.suggestion.dto.SuggestionDto;
+import swm.hkcc.LGTM.app.modules.suggestion.exception.NotExistSuggestion;
 import swm.hkcc.LGTM.app.modules.suggestion.repository.SuggestionLikeRepository;
 import swm.hkcc.LGTM.app.modules.suggestion.repository.SuggestionRepository;
+
+import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,6 +29,26 @@ public class SuggestionService {
         Suggestion suggestion = Suggestion.from(requestBody, member);
         suggestionRepository.save(suggestion);
         return CreateSuggestionResponse.from(suggestion);
+    }
+
+    public List<SuggestionDto> getSuggestionList(Member member) {
+        List<Suggestion> suggestionList = suggestionRepository.findAll(
+                Sort.by(Sort.Direction.DESC, "createdAt"));
+        return suggestionList.stream()
+                .map(suggestion -> getSuggestionDetail(suggestion, member))
+                .toList();
+    }
+
+    public SuggestionDto getSuggestion(Long suggestionId, Member member) {
+        Suggestion suggestion = suggestionRepository.findById(suggestionId).orElseThrow(NotExistSuggestion::new);
+        return getSuggestionDetail(suggestion, member);
+    }
+
+    private SuggestionDto getSuggestionDetail(Suggestion suggestion, Member member) {
+        return SuggestionDto.from(suggestion,
+                suggestionLikeRepository.countBySuggestion_SuggestionId(suggestion.getSuggestionId()),
+                suggestionLikeRepository.existsBySuggestion_SuggestionIdAndMember_MemberId(suggestion.getSuggestionId(), member.getMemberId()),
+                Objects.equals(member.getMemberId(), suggestion.getWriter().getMemberId()));
     }
 
 }
