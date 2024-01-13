bf17ce0d4dfd0c8b76c76efd3573e5b9e5d34a1d
Yoonho Cho (Ray) <raycho521@yonsei.ac.kr>
2024-01-13 13:22:52 +0900

    feat #152: add repositories

--- API-Server/src/main/java/swm/hkcc/LGTM/app/modules/suggestion/repository/SuggestionLikeRepository.java
+++ API-Server/src/main/java/swm/hkcc/LGTM/app/modules/suggestion/repository/SuggestionLikeRepository.java
@@ -3,7 +3,12 @@ package swm.hkcc.LGTM.app.modules.suggestion.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import swm.hkcc.LGTM.app.modules.suggestion.domain.SuggestionLike;
 
+import java.util.Optional;
+
 public interface SuggestionLikeRepository extends JpaRepository<SuggestionLike, Long> {
     Integer countBySuggestion_SuggestionId(Long suggestionId);
+
     Boolean existsBySuggestion_SuggestionIdAndMember_MemberId(Long suggestionId, Long memberId);
+
+    Optional<SuggestionLike> findBySuggestion_SuggestionIdAndMember_MemberId(Long suggestionId, Long memberId);
 }
